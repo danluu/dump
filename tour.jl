23815022a8be435ddb76eb0dae9665c4e8ed3259
Leah Hanson <astrieanna@gmail.com>
2013-04-24 12:07:39 -0400

    make is_rotation more readable by changing x^2 to  and making the function one line.

--- tour.jl
+++ tour.jl
@@ -31,12 +31,7 @@ end
 assert(sub_spaces("this is a string   --yep") == "this%20is%20a%20string%20%20%20--yep")
 
 function is_rotation(x::String, y::String)
-    if length(x) == length(y)
-        xx = x^2
-        return ismatch(Regex(y),xx)
-    else
-        return false
-    end 
+    return length(x)==length(y) && ismatch(Regex(y),"$x$x")
 end
 
 assert(is_rotation("waterbottle", "erbottlewat") == true)
