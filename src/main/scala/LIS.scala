cb75ab87c89b64a016087fd8b3fb0fc9e6809f85
Dan Luu <danluu@gmail.com>
2013-04-24 23:25:02 -0400

    Add LIS

---
+++ src/main/scala/LIS.scala
@@ -0,0 +1,32 @@
+package snippets.algorithms
+
+object LIS {
+  def main(args: Array[String]) {
+    println(solve(Array(7,3,8,4,2,6,7)))
+  }
+
+  // 1-d dynamic programming
+  // keep track of all lengths. New max is max of all previous + 1,
+  // for all elements that are < current
+  def solve(xs: Array[Int]) = {
+    val len = Array.fill(xs.length){0}
+    val prev = Array.fill(xs.length){0}
+    (1 until xs.length).foreach{ i =>
+      val sublen = len.slice(0,i).zipWithIndex.map{ x =>
+        if(xs(x._2) < xs(i)) //prev < current, so inc prev longest and save the index
+          (1 + x._1, x._2)
+        else 
+          (1, i)
+      }
+      len(i) = sublen.maxBy(_._1)._1
+      prev(i) = sublen.maxBy(_._1)._2
+    }
+    println(prev.mkString(" "))
+    len.max
+  }
+
+  def solveRecursive(xs: Array[Int]) = {
+
+  }
+
+}
