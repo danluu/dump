b36088cea055d0b0b5da838a9c5f20cf14e51515
Dan Luu <danluu@gmail.com>
2013-05-01 15:30:39 -0400

    Add run logic

--- src/main/scala/Regex.scala
+++ src/main/scala/Regex.scala
@@ -9,7 +9,7 @@ case class Union(left: Regex, right: Regex) extends Regex
 case class Concat(right: Regex, left: Regex) extends Regex
 case class Star(x: Regex) extends Regex
 
-object Foo{
+object RegexEngine{
   def success(x: Regex): Boolean = x match {
     case Nil => false
     case Empty => true
@@ -34,5 +34,12 @@ object Foo{
       else 
         Concat(step(left, c), right)
     case Star(regexX) => Concat(step(regexX, c), Star(regexX))
+  }
+
+  def run(x: Regex, s: String): Regex = s.foldLeft(x){(x,c) => step(x,c)}
+  def matches(x: Regex, s: String): Boolean = success(run(x,s))
+
+  def main(args: Array[String]) = {
+    
   }
 }
