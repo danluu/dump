c66634e273865da999bc8af1ba305239490f4cf2
Dan Luu <danluu@gmail.com>
2013-05-03 18:53:13 -0400

    Move tests to real test file

--- build.sbt
+++ build.sbt
@@ -3,3 +3,9 @@ name := "algorithms"
 version := "1.0"
 
 scalaVersion := "2.10.0"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.9.1" % "test"
+
+libraryDependencies ++= Seq(
+   "junit" % "junit" % "4.8.1" % "test"
+)
---
+++ src/test/scala/RegexSpec.scala
@@ -0,0 +1,42 @@
+package snippets.algorithms
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class TreeSuite extends FunSuite {
+  import RegexEngine._
+  test("Simple Regex Tests"){
+    val a = Lit('a')
+    val b = Lit('b')
+    assert(matches(a, "a") == true)
+    assert(matches(a, "aa") == false) // This is a classical regex
+    val aStar = Star(a)
+    assert(matches(aStar, "") == true)
+    assert(matches(aStar, "a") == true)
+    assert(matches(aStar, "aa") == true)
+    val aPlus = Concat(a, aStar)
+    assert(matches(aPlus, "") == false)
+    assert(matches(aPlus, "a") == true)
+    assert(matches(aPlus, "aa") == true)
+    val aStarb = Concat(aStar, b)
+    assert(matches(aStarb, "b") == true)
+    assert(matches(aStarb, "ab") == true)
+    assert(matches(aStarb, "aab") == true)
+    assert(matches(aStarb, "aaba") == false)
+    val aStara = Concat(aStar, a)
+    assert(matches(aStara, "a") == true)
+    assert(matches(aStara, "aa") == true)
+    assert(matches(aStara, "aaa") == true)
+    val aStarab = Concat(aStara, b)
+    assert(matches(aStarab, "ab") == true)
+    assert(matches(aStarab, "aab") == true)
+    assert(matches(aStarab, "aaab") == true)
+    assert(matches(aStarab, "aaa") == false)
+    val ab = Concat(a, b)
+    val abStar = Star(ab)
+    println(matches(abStar,"a"))
+  }
+}
