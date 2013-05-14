a9e1956dc15400c1ad8e0ce208793c7e6408511f
Dan Luu <danluu@gmail.com>
2013-05-14 12:55:37 -0400

    Add immutable version

--- src/main/scala/MultiTree.scala
+++ src/main/scala/MultiTree.scala
@@ -6,6 +6,10 @@ sealed trait MultiTree
 case class Leaf(x: Int) extends MultiTree
 case class MNode(nodes: List[MultiTree]) extends MultiTree
 
+sealed trait PolyReturn
+case class Continue(x: Int) extends PolyReturn
+case class Found(x: Int) extends PolyReturn
+
 case class FoundMatch(message: String = null, cause: Throwable = null, item: Int) extends RuntimeException(message, cause)
 
 object MTreeToy {
@@ -15,8 +19,12 @@ object MTreeToy {
         MNode(List
           (Leaf(3), Leaf(4))),
         Leaf(5), Leaf(6)))))
-    println(find(t, 3))
+    (0 to 7).foreach{i =>
+      println(find(t,i))
+      println(findImmutable(t,i))
+    }
   }
+
 
   def find(t: MultiTree, x: Int) = {
     try {
@@ -40,5 +48,27 @@ object MTreeToy {
         i = i + temp
       }
       i
+  }
+
+  // inspiried by Raul
+  def findImmutable(t: MultiTree, x: Int) = {
+    def findImmutableHelper(t: MultiTree, x: Int): PolyReturn = t match {
+      case Leaf(y) =>
+        if (x == 0)
+          Found(y)
+        else
+          Continue(x - 1)
+      case MNode(List()) =>
+        Continue(x)
+      case MNode(nodes) =>
+        findImmutableHelper(nodes.head, x) match {
+        case Found(y) => Found(y)
+          case Continue(x)  => findImmutableHelper(MNode(nodes.tail), x)
+        }
+    }
+    findImmutableHelper(t, x) match {
+      case Found(x) => Some(x)
+      case _ => None
+    }
   }
 }
