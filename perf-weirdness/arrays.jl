718b93fe6acc458dbbe3bd481cb75f3cebf19c7f
Dan Luu <danluu@gmail.com>
2014-07-31 20:02:08 -0500

    Add c version

--- perf-weirdness/arrays.jl
+++ perf-weirdness/arrays.jl
@@ -1,16 +1,14 @@
 function setup(a::Array, n::Int)
     for k in 1:n, j in 1:n, i in 1:n
-        a[i,j,k] = i*3j*5k + 2i + 7j
+        a[i,j,k] = 11*i*j*k + 2i + 7j
     end
 end
 
 function sum_array(a::Array, n::Int)
-    sum = 0
     for k in 2:n, j in 1:n, i in 1:n
         a[i,j,k] = min(a[i,j,k-1],
                        a[i,k,k-1] + a[k,j,k-1])
     end
-    return sum
 end
 
 function main(n::Int)    
