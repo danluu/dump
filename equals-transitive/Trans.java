77b5d1d707e14d89ab25dec28a8634324c5d2fe3
Dan Luu <danluu@gmail.com>
2014-08-13 19:30:10 -0500

    Add java test.

--- equals-transitive/Trans.java
+++ equals-transitive/Trans.java
@@ -1,5 +1,5 @@
 class Trans {
-    public static void main(String args[]) {
+    public static void mainruby (String args[]) {
 	long   x = 9007199254740992L;    // 2^53
 	double y = 9007199254740992.0;  // 2.0^53
 	long   z = 9007199254740993L;    // 2^53+1
