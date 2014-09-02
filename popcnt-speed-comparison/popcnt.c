89ed3f6fc1d09cc2131f149915c84352823f17e9
Dan Luu <danluu@gmail.com>
2014-09-01 19:48:44 -0500

    Add version that does SSE loads.

--- popcnt-speed-comparison/popcnt.c
+++ popcnt-speed-comparison/popcnt.c
@@ -18,7 +18,7 @@ void setup_buffer(int len) {
   buffer[len-1] = 3;
 }
 
-// Note that this emits a popcnt with gcc but not clang.
+// Note: this emits a popcnt with clang 3.4 but not with clang 3.0
 uint32_t builtin_popcnt(const uint64_t* buf, int len) {
   int cnt = 0;
   for (int i = 0; i < len; ++i) {
@@ -42,6 +42,7 @@ uint32_t builtin_popcnt_unrolled(const uint64_t* buf, int len) {
 // Attempt to work around false depdency errata.
 // gcc is too smart to fall for this and re-creates the dependency unless
 // compiled with -funroll-loops or something similar.
+// This works with clang, though.
 uint32_t builtin_popcnt_unrolled_errata(const uint64_t* buf, int len) {
   assert(len % 4 == 0);
   int cnt[4];
@@ -56,6 +57,52 @@ uint32_t builtin_popcnt_unrolled_errata(const uint64_t* buf, int len) {
     cnt[3] += __builtin_popcountll(buf[i+3]);
   }
   return cnt[0] + cnt[1] + cnt[2] + cnt[3];
+}
+
+uint32_t builtin_popcnt_movdq(const uint64_t* buf, int len) {
+  int cnt = 0;
+  __m128i temp;
+  __m128i temp2;
+  uint64_t lower64;
+  uint64_t upper64;
+
+  for (int i = 0; i < len; i+=2) {
+    temp = _mm_load_si128((__m128i*)&buf[i]);
+    lower64 = _mm_cvtsi128_si64(temp);
+    cnt += __builtin_popcountll(lower64);
+    temp2 = (__m128i)_mm_movehl_ps((__m128)temp, (__m128)temp);
+    upper64 = _mm_cvtsi128_si64(temp2);
+    cnt += __builtin_popcountll(upper64);
+  }
+  return cnt;
+}
+
+uint32_t builtin_popcnt_movdq_unrolled(const uint64_t* buf, int len) {
+  int cnt[2];
+  __m128i temp[2];
+  __m128i temp_upper[2];
+  uint64_t lower64[2];
+  uint64_t upper64[2];
+
+  for (int i = 0; i < 2; ++i) {
+    cnt[i] = 0;
+  }
+
+  for (int i = 0; i < len; i+=4) {
+    temp[0] = _mm_load_si128((__m128i*)&buf[i]);
+    temp[1] = _mm_load_si128((__m128i*)&buf[i+2]);
+    lower64[0] = _mm_cvtsi128_si64(temp[0]);
+    lower64[1] = _mm_cvtsi128_si64(temp[1]);
+    cnt[0] += __builtin_popcountll(lower64[0]);
+    cnt[1] += __builtin_popcountll(lower64[1]);
+    temp_upper[0] = (__m128i)_mm_movehl_ps((__m128)temp[0], (__m128)temp[0]);
+    temp_upper[1] = (__m128i)_mm_movehl_ps((__m128)temp[1], (__m128)temp[1]);
+    upper64[0] = _mm_cvtsi128_si64(temp_upper[0]);
+    upper64[1] = _mm_cvtsi128_si64(temp_upper[1]);
+    cnt[0] += __builtin_popcountll(upper64[0]);
+    cnt[1] += __builtin_popcountll(upper64[1]);
+  }
+  return cnt[0] + cnt[1];
 }
 
 // TODO: refactor the following fns into a single fn that takes a function
@@ -118,6 +165,52 @@ int run_builtin_popcnt_unrolled_errata(int len, int iterations) {
   for (int i = 0; i < iterations; ++i) {
     RDTSC_START(tsc_before);
     total += builtin_popcnt_unrolled_errata(buffer, len);
+    RDTSC_STOP(tsc_after);
+    tsc = tsc_after - tsc_before;
+    min_tsc = min_tsc < tsc ? min_tsc : tsc;
+  }
+
+  //  assert(total == iterations * 3); // Check that we don't have an off by one error.
+
+  asm volatile("" :: "m" (total));
+  return min_tsc;
+}
+
+int run_builtin_popcnt_movdq(int len, int iterations) {
+
+  uint32_t total = 0;
+  uint64_t tsc_before, tsc_after, tsc, min_tsc;
+  min_tsc = 0;
+  min_tsc--;
+
+  asm volatile("" :: "m" (buffer[0]));
+
+  for (int i = 0; i < iterations; ++i) {
+    RDTSC_START(tsc_before);
+    total += builtin_popcnt_movdq(buffer, len);
+    RDTSC_STOP(tsc_after);
+    tsc = tsc_after - tsc_before;
+    min_tsc = min_tsc < tsc ? min_tsc : tsc;
+  }
+
+  //  assert(total == iterations * 3); // Check that we don't have an off by one error.
+
+  asm volatile("" :: "m" (total));
+  return min_tsc;
+}
+
+int run_builtin_popcnt_movdq_unrolled(int len, int iterations) {
+
+  uint32_t total = 0;
+  uint64_t tsc_before, tsc_after, tsc, min_tsc;
+  min_tsc = 0;
+  min_tsc--;
+
+  asm volatile("" :: "m" (buffer[0]));
+
+  for (int i = 0; i < iterations; ++i) {
+    RDTSC_START(tsc_before);
+    total += builtin_popcnt_movdq_unrolled(buffer, len);
     RDTSC_STOP(tsc_after);
     tsc = tsc_after - tsc_before;
     min_tsc = min_tsc < tsc ? min_tsc : tsc;
@@ -258,6 +351,8 @@ int main() {
     printf("builtin: %i\n", run_builtin_popcnt(len, ITERATIONS));
     printf("builtin unrolled: %i\n", run_builtin_popcnt_unrolled(len, ITERATIONS));
     printf("builtin errata: %i\n", run_builtin_popcnt_unrolled_errata(len, ITERATIONS));
+    printf("builtin movdq: %i\n", run_builtin_popcnt_movdq(len, ITERATIONS));
+    printf("builtin movdq unrolled: %i\n", run_builtin_popcnt_movdq_unrolled(len, ITERATIONS));
     printf("SSSE3: %i\n", run_mula_popcnt(len, ITERATIONS));
   }
 }
