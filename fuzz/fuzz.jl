3dd0021095feb4fe31c42e5c1e38f30dcbd8be6d
Dan Luu <danluu@gmail.com>
2014-09-04 18:49:36 -0500

    Add world's simplest fuzzer. It found a julia segfault bug, though.

---
+++ fuzz/fuzz.jl
@@ -0,0 +1,12 @@
+const test_dir = "jl_input"
+
+function generate_tests(n::Int)
+    for i in 1:n
+        len = rand(1:10000000)
+        f = open("$(test_dir)/$(i)","w")
+        write(f, randstring(len))
+        close(f)
+    end
+end
+
+generate_tests(3)
