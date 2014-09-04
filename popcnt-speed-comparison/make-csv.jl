be53ad44859878216a7097b272ab06b77c1fb11c
Dan Luu <danluu@gmail.com>
2014-09-03 22:15:50 -0500

    Try adding header.

--- popcnt-speed-comparison/make-csv.jl
+++ popcnt-speed-comparison/make-csv.jl
@@ -14,6 +14,26 @@ function get_data_hash()
     return h
 end
 
+function print_header(h, delta::Int)
+    row_length = -1
+    some_row = Array(Int)
+    for (_, row) in h
+        if row_length == -1
+            row_length = length(row)
+        else
+            assert(row_length == length(row))
+        end
+    end
+    
+    print("bytes")
+    current = 0
+    for i in 1:row_length
+        current += delta * 8
+        print(",$(current))")
+    end
+    print("\n")
+end
+
 function print_data_hash(h)
     for (name, times) in h
         print("$name,$(join(times,','))\n")
@@ -21,4 +41,5 @@ function print_data_hash(h)
 end
 
 h = get_data_hash()
+print_header(h, 128)
 print_data_hash(h)
