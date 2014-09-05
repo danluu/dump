de0b20a698629d4926ff9ec6de627e8ab0431dd9
Dan Luu <danluu@gmail.com>
2014-09-05 11:38:27 -0500

    Add test to see if pcregrep bug is reproducible using pcre bindings.

--- fuzz/shrink.jl
+++ fuzz/shrink.jl
@@ -1,5 +1,6 @@
 const test_dir = "jl_input"
 
+# Find shortest too-long symbol bug.
 function generate_tests(n::Int)
     for i in 1:n
         len = 2^i
@@ -9,4 +10,21 @@ function generate_tests(n::Int)
     end
 end
 
-generate_tests(20)
+# generate_tests(20)
+
+# find shorter PCRE bug.
+# This turned out to be a bug in pcregrep and not pcre itself.
+function foo()
+    prefix_len = rand(0:8000000)
+    postfix_len = rand(0:80)
+    text = "not defined"
+    regex = r"not defined"
+    dut = string(randstring(prefix_len),text,randstring(postfix_len))
+    if !ismatch(regex, dut)
+        print("ERROR: $dut")
+    end
+end
+
+while true
+    foo()
+end
