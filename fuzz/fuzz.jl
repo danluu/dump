91e7de9586922b70526fdac85ea0bf8ded25a1fe
Dan Luu <danluu@gmail.com>
2014-09-05 15:52:05 -0500

    Add string test.

--- fuzz/fuzz.jl
+++ fuzz/fuzz.jl
@@ -1,6 +1,6 @@
 const test_dir = "jl_input"
 
-function generate_tests(n::Int)
+function generate_rand_strings(n::Int)
     for i in 1:n
         len = rand(1:10000000)
         f = open("$(test_dir)/$(i)","w")
@@ -9,4 +9,4 @@ function generate_tests(n::Int)
     end
 end
 
-generate_tests(3)
+generate_rand_strings(3)
--- fuzz/shrink.jl
+++ fuzz/shrink.jl
@@ -1,12 +1,21 @@
 const test_dir = "jl_input"
 
-function generate_tests(n::Int)
+# Note that it takes a really long time for julia
+# to construct a huge string. Seems like a super
+# low priority performance bug to debug someday.
+function generate_tests(n::Int, string::Bool)
     for i in 1:n
         len = 2^i
         f = open("$(test_dir)/$(i)","w")
+        if string
+            write(f,"\"")
+        end
         write(f, randstring(len))
+        if string
+            write(f,"\"")
+        end
         close(f)
     end
 end
 
-generate_tests(20)
+generate_tests(20, false)
