f0f30109bc40880689fe9d36d3c555d834366c3e
Dan Luu <danluu@gmail.com>
2014-09-08 12:46:06 -0500

    More exception experiments.

--- fuzz/peakflops.jl
+++ fuzz/peakflops.jl
@@ -1,6 +1,6 @@
 # peakflops often seems to cause a hang. What happens if we just try it a lot?
 
-function wheee()
+function wheee0()
     i = 0
     while true
         try
@@ -10,11 +10,57 @@ function wheee()
             i += 1
             peakflops(1954784290346684782)
         catch
+            print(i)
         end
+    end
+end
+
+function wheee1()
+    i = 0
+    while true
+        try
+            i += 1
+            if i == 1
+                print(".")
+            end
+            peakflops(1954784290346684782)
+        catch
+            print(i)
+        end
+    end
+end
+
+function mid_inc()
+    i = 0
+    try
+        if i == 0
+            print(".")
+        end
+        i += 1
+        peakflops(1954784290346684782)
+    catch
+        print(i)
+    end
+end
+
+
+function pre_inc()
+    i = 0
+    try
+        i += 1
+        if i == 1
+            print(".")
+        end
+        peakflops(1954784290346684782)
+    catch
+        print(i)
     end
 end
 
 
 # print(code_typed(wheee, ()))
-print(code_llvm(wheee, ()))
-# wheee()
+# print(code_llvm(wheee, ()))
+
+# mid_inc()
+# pre_inc()
+
