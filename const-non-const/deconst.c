37e4adae3229fc5f1519ae9b64c9718f9736879e
Dan Luu <danluu@gmail.com>
2014-10-12 10:12:39 -0500

    Experiment with undefiend const behavior.

---
+++ const-non-const/deconst.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void wat(int* mutable) {
+  mutable += 1;
+}
+
+void wat2(int* mutable) {
+  *mutable = 100;
+}
+
+int main() {
+  const int immutable = 10;
+  printf("%i\n", immutable);
+  wat((int*) &immutable);
+  printf("%i\n", immutable);
+  wat2((int*) &immutable);
+  printf("%i\n", immutable);
+}
