2ac39a5fd87cd30f5c6c77ffc66de82cac1a942f
Dan Luu <danluu@gmail.com>
2014-10-13 16:41:02 -0500

    Make fn for checking x-nearness

--- lemmings/lemmings.elm
+++ lemmings/lemmings.elm
@@ -6,13 +6,13 @@ mario = { x=0, y=500, vx=0, vy=0, dir="right" }
 
 platform = { xmin=-250, xmax=200, y = 250 }
 
+near m platform = m.x < platform.xmax && m.x > platform.xmin
 
 -- UPDATE -- ("m" is for Mario)
 jump {y} m = if y > 0 && m.y == 0 then { m | vy <- 5 } else m
 gravity t m = 
   if ((m.y <= platform.y && m.y - t*m.vy >= platform.y) && 
-    m.x < platform.xmax && 
-    m.x > platform.xmin)
+    near m platform)
     then {m | vy <- 0, y <- platform.y}
   else if m.y <= 0
     then {m | vy <- 0, y <- 0}
@@ -20,7 +20,7 @@ gravity t m =
     { m | vy <- m.vy - t/4 }
 physics t m = { m | x <- m.x + t*m.vx , y <- max 0 (m.y + t*m.vy) }
 walk {x} m = 
-  if m.vx == 0 && (m.y == 0 || (m.y == platform.y && m.x < platform.xmax && m.x > platform.xmin))
+  if m.vx == 0 && (m.y == 0 || (m.y == platform.y && near m platform))
     then { m | vx <- 2}
   else if m.x > 300 && m.y == 0
     then { m | vx <- -2}
