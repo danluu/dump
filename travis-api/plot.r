123d41ac6379961ebdb89feccdb081a1989e624b
Dan Luu <danluu@gmail.com>
2014-11-02 11:43:45 -0600

    Zero out scale.

--- travis-api/plot.r
+++ travis-api/plot.r
@@ -5,7 +5,7 @@ png(filename="reliability.png",width=640,height=1280)
 df <- read.csv(file="results.csv")
 ggplot(data=df,aes(x=reorder(project, nines),fill=category,y=nines)) + geom_bar() + coord_flip() +
 labs(y="Uptime (percent)", x="Project") +
-scale_y_continuous(breaks=c(.1,1,2,3,4,5), labels=c("9","90","99","99.9","99.99","99.999")) +
+scale_y_continuous(breaks=c(0,1,2,3,4,5), labels=c("0","90","99","99.9","99.99","99.999")) +
 theme_minimal(base_size=16) +
 theme(legend.justification=c(1,0), legend.position=c(1,.5)) +
 scale_fill_brewer(palette="Set1")
