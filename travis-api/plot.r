b10c67d8de48dfce7827c12cb380b3d8c176b108
Dan Luu <danluu@gmail.com>
2014-11-01 22:33:31 -0500

    Tweak plot names.

--- travis-api/plot.r
+++ travis-api/plot.r
@@ -6,7 +6,7 @@ df <- read.csv(file="results.csv")
 qplot(data=df,x=reorder(project, nines),fill=category,y=nines) + geom_bar() + coord_flip() +
 labs(y="Uptime (percent)", x="Project") +
 scale_y_continuous(breaks=c(.1,1,2,3,4,5), labels=c("9","90","99","99.9","99.99","99.999")) + 
-theme_minimal(base_size=12) +
+theme_minimal(base_size=16) +
 theme(legend.justification=c(1,0), legend.position=c(1,.5)) + 
 scale_fill_brewer(palette="Set1")
 
