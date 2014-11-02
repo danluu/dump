325063b638b437c3a4a7a83c2e1fd4c6f64f8be1
Dan Luu <danluu@gmail.com>
2014-11-01 21:55:47 -0500

    Fix up plotting details.

--- travis-api/plot.r
+++ travis-api/plot.r
@@ -1,4 +1,13 @@
 setwd("~/dev/dump/travis-api")
 
+png(filename="reliability.png",width=640,height=1280)
+
 df <- read.csv(file="results.csv")
-qplot(data=df,x=reorder(project, nines),fill=category,y=nines) + geom_bar() + coord_flip()
+qplot(data=df,x=reorder(project, nines),fill=category,y=nines) + geom_bar() + coord_flip() +
+labs(y="Uptime (percent)", x="Project") +
+scale_y_continuous(breaks=c(.1,1,2,3,4,5), labels=c("9","90","99","99.9","99.99","99.999")) + 
+theme_minimal(base_size=12) +
+theme(legend.justification=c(1,0), legend.position=c(1,.5)) + 
+scale_fill_brewer(palette="Set1")
+
+dev.off()
