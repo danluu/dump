a95f310d5cb5d14de346594b526901f56851ffbd
Dan Luu <danluu@gmail.com>
2014-11-01 20:16:49 -0500

    Replace terrible ad hoc thing with reasonable formula.

--- travis-api/parse.jl
+++ travis-api/parse.jl
@@ -3,51 +3,9 @@ using Dates
 # Example usage: julia parse.jl cleaned/devops devops
 # Produces category,name,pass_rate,fail_rate,error_rate
 
-# This number of 9s nonsense is never used. Just playing with something.
-# Convert a percent to colloqial "9s" measurement, as in 3 9s of reliability.
+
 function number_of_9s(pct::Float64)
-    if pct >= 1.0
-        return float64(Inf)
-    end
-    
-    base,remainder = base_number_of_9s(pct)
-    if isnan(base) || isnan(remainder)
-        return NaN
-    end
-
-    if base >= 0
-        # println("log of $(remainder *10^(base+1)) is $(log(9,remainder * 10^(base+1)))")
-        fraction = log(9, remainder * 10^(base+1))
-        fraction = fraction < 0 ? 0 : fraction # Horrible hack for our weirdo not-quite log scale.
-    else
-        assert(false) # Shouldn't happen with our data.
-    end
-    return base + fraction
-end
-
-# Floor of number of 9s, i.e., number of 9s without fractional part.
-# This is the worst hack ever written. I apologize to anyone reading this.
-function base_number_of_9s(pct::Float64)
-    if isnan(pct)
-        return (NaN, pct)
-    end
-
-    assert(pct > .09)
-    if pct < .9
-        return (0, pct)
-    elseif pct < .99
-        return (1, pct-.9)
-    elseif pct < .999
-        return (2, pct-.99)
-    elseif pct < .9999
-        return (3, pct-.999)
-    elseif pct < .99999
-        return (4, pct-.9999)
-    elseif pct < .999999
-        return (5, pct-.99999)
-    elseif pct < .9999999
-        assert(false) # We don't ever hit this with our data.
-    end
+    log(10, 1 / (1.0 - pct))
 end
 
 function process_travis_log(fname::String, category::String, project::String)
