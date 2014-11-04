66835df6951761a83aa456195746f6de610eb286
Dan Luu <danluu@gmail.com>
2014-11-03 20:52:36 -0600

    Add plot of nulls.

--- linux-mining/plot.r
+++ linux-mining/plot.r
@@ -2,4 +2,6 @@ library(ggplot2)
 
 setwd("~/dev/dump/linux-mining")
 
+png(filename="null-percentile.png",width=640,height=480)
 df <- read.csv(file="nulls.csv")
+ggplot(df,aes(Rank,Percentile)) + geom_point(alpha=1/10)
