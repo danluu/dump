2dcc2f7b04201fe5559c5f871ce0cdb4b33a9404
Dan Luu <danluu@gmail.com>
2014-11-03 21:00:02 -0600

    Add script to generate table from csv.

---
+++ linux-mining/make-table.jl
@@ -0,0 +1,20 @@
+# Convert first N columns of a csv to markdown table format.
+
+function read_csv()
+    fname = "filtered.csv"
+    max_col = 6
+    max_row = 40
+    
+    f = open(fname)
+    line = readline(f)
+    line_num = 1
+    while line != "" && line_num <= max_row
+        chunks = split(line,',')
+        middle_cols = join(chunks[1:max_col], " | ") # assume max_col <= length
+        println("| $middle_cols |")
+        line = readline(f)
+        line_num += 1
+    end
+end
+
+read_csv()
