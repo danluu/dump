15970dc69a4900dbd2c0f4bb71969ff72e2578b3
Dan Luu <danluu@gmail.com>
2014-11-15 09:13:33 -0600

    Use macro.

--- rust-experiments/syscall_test/src/main.rs
+++ rust-experiments/syscall_test/src/main.rs
@@ -1,5 +1,15 @@
+#![feature(phase)]
+
+#[phase(plugin, link)]
 extern crate syscall;
+
+fn write(fd: uint, buf: &[u8]) {
+    unsafe {
+        syscall!(WRITE, fd, buf.as_ptr(), buf.len());
+    }
+}
 
 fn main() {
     println!("Hello, world!");
+    write(1, "Hello, world via syscall!\n".as_bytes());
 }
