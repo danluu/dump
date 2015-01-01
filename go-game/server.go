3a19c8f2f18db1b94822166279065b4940ca19ad
Dan Luu <danluu@gmail.com>
2015-01-01 15:23:19 -0600

    Really separate game and server, sort of. It's still not a good separation.

--- go-game/server.go
+++ go-game/server.go
@@ -1,10 +1,140 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
+
+const (
+	writeWait = 10 * time.Second
+	pongWait = 60 * time.Second
+	pingPeriod = (pongWait * 9) / 10
+	maxMessageSize = 512
+)
+
+func assert(cond bool, message string) {
+	if !cond {
+		fmt.Println("assert failed: " + message)
+	}
+}
+
+type GameMessage struct {
+	Message string
+	Player int
+	Cards []string
+}
+
+// send is the message from the hub we want to send to the websocket.
+type connection struct {
+	websocket *websocket.Conn
+	send chan GameMessage
+}
+
+func (conn *connection) fromBrowser() {
+	defer func() {
+		globalHub.unregister <- conn
+		conn.websocket.Close()
+	}()
+	conn.websocket.SetReadLimit(maxMessageSize)
+	conn.websocket.SetReadDeadline(time.Now().Add(pongWait))
+	conn.websocket.SetPongHandler(func(string) error { conn.websocket.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	for {
+		_, message, err := conn.websocket.ReadMessage()
+		if err != nil {
+			break
+		}
+		globalHub.broadcast <- message
+	}
+}
+
+func (conn *connection) write(messageType int, payload []byte) error {
+	conn.websocket.SetWriteDeadline(time.Now().Add(writeWait))
+	return conn.websocket.WriteMessage(messageType, payload)
+}
+
+func (conn *connection) writeJSON(message GameMessage) error {
+	conn.websocket.SetWriteDeadline(time.Now().Add(writeWait))
+	return conn.websocket.WriteJSON(message)
+}
+
+func (conn *connection) toBrowser() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		conn.websocket.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-conn.send:
+			if !ok {
+				conn.write(websocket.CloseMessage, []byte{})
+				return
+			}
+			if err := conn.writeJSON(message); err != nil {
+				return
+			}
+		case <-ticker.C:
+			if err := conn.write(websocket.PingMessage, []byte{}); err != nil {
+				return
+			}
+		}
+	}
+}
+
+type hub struct {
+	broadcast chan []byte
+	connections map[*connection]bool
+	ready chan bool
+	register chan *connection
+	unregister chan *connection
+}
+
+var globalHub = hub{
+	broadcast:   make(chan []byte),
+	connections: make(map[*connection]bool),
+	ready:       make(chan bool),
+	register:    make(chan *connection),
+	unregister:  make(chan *connection),
+}
+
+func sendToAll(all hub, message GameMessage) {
+	for c := range all.connections {
+		select {
+		case c.send <- message:
+		default:
+			close(c.send)
+			delete(all.connections, c)
+		}
+	}
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func wsHandler(response http.ResponseWriter, request *http.Request) {
+	if request.Method != "GET" {
+		http.Error(response, "Method not allowed", 405)
+		return
+	}
+	
+	wsConnection, err := upgrader.Upgrade(response, request, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	conn := &connection{send: make(chan GameMessage, 256), websocket: wsConnection}
+	globalHub.register <- conn
+	go conn.toBrowser()
+	conn.fromBrowser()
+}
 
 func main() {
 	go globalHub.run()
