712557e5cf0815bc168253b60b7ed9335f088d42
Dan Luu <danluu@gmail.com>
2015-01-23 12:19:22 -0600

    An attempt at speeding up.

--- usaco/1.4/ariprog.cc
+++ usaco/1.4/ariprog.cc
@@ -17,120 +17,35 @@ LANG: C++11
 #include <set>
 #include <sstream>
 #include <string>
+#include <tuple>
 #include <unordered_set>
 #include <vector>
 
 using namespace std;
 
-#define HUGE_NUM 250*250*2
-
-//int bisquares[HUGE_NUM];
-
-unordered_set<int> find_bisquares(int m) {
-  unordered_set<int> bisquares;
-  for (int i = 0; i <= m; ++i) {
-    for (int j = i; j <= m; ++j) {
-      // cout << "bs:" << i*i + j*j << endl;
-      bisquares.insert(i*i + j*j);
-    }
+int cmp_pairs(const pair<int,int>& left, const pair<int,int>& right) {
+  if (left.second < right.second) {
+    return 1;
+  } else if (left.second == right.second) {
+    return left.first < right.first;
+  } else {
+    return 0;
   }
-  return bisquares;
+  assert(0);
 }
-
-/*
-void set_bisquares(int m) {
-  memset(bisquares, 0, HUGE_NUM * sizeof(int));
-  for (int i = 0; i <= m; ++i) {
-    for (int j = i; j <= m; ++j) {
-      bisquares[i*i + j*j] = 1;
-    }
-  }
-}
-*/
 
 vector<int> create_bisquares(int m) {
-  vector<int> bisquares;
+  set<int> unique_bisquares;
   for (int i = 0; i <= m; ++i) {
     for (int j = i; j <= m; ++j) {
-      bisquares.push_back(i*i + j*j);
+      unique_bisquares.insert(i*i + j*j);
     }
   }
-  sort(bisquares.begin(), bisquares.end());
+  vector<int> bisquares(unique_bisquares.begin(), unique_bisquares.end());
   return bisquares;
 }
 
-
-int check_diff_len_start(const unordered_set<int>& bisquares, int diff, int n, int start) {
-  int current = start;
-  for (int i = 0; i < n; ++i) {
-    if (bisquares.find(current) == bisquares.end()) {
-      // cout << "diff n start " << diff << " " << n << " " << start << endl;
-      // cout << "failed at " << i << endl;
-      return 0;
-    }
-    current += diff;
-  }
-  return 1;
-}
-
-// Need to change lengths first, starting value second.
-void solve(int n, int m, ofstream& fout) {
-  auto bisqures = find_bisquares(m);
-  cout << "#bisquares " << bisqures.size() << endl;
-  int found_one = 0;
-  
-  // i = delta, j = starting val
-  for (int i = 1; i <= 2*m*m; ++i) {
-    for (int j = 0; j <= 2*m*m; ++j) {
-      if (check_diff_len_start(bisqures, i, n, j)) {
-	found_one = 1;
-	fout << j << " " << i << endl;
-      }
-    }
-  }
-
-  if (!found_one) {
-    fout << "NONE\n";
-  }
-}
-
-/*
-void solve_faster(int n, int m, ofstream& fout) {
-  set_bisquares(m);
-  int found_one = 0;
-  
-  // i = delta, j = starting val
-  for (int i = 1; i <= 2*m*m; ++i) {
-    vector<int> ans;
-    for (int offset = 0; offset < i; ++offset) {
-      int run_length = 0;
-      for (int j = offset; j <= 2*m*m; j+=i) {
-	if (bisquares[j]) {
-	  ++run_length;
-	  //	  cout << "delta " << i << "j " << j << " " << run_length << endl;
-	  if (run_length >= n) {
-	    found_one = 1;
-	    // fout << j - (n-1)*i << " " << i << endl;	  
-	    ans.push_back(j - (n-1)*i);
-	  }
-	} else {
-	  run_length = 0;
-	}
-      }
-    }
-    sort(ans.begin(), ans.end());
-    for (auto& start : ans) {
-      fout << start << " " << i << endl;
-    }
-  }
-
-  if (!found_one) {
-    fout << "NONE\n";
-  }
-}
-*/
-
-int seq_starts_at(vector<int>& bisquares, int diff, int n, int start) {
+int seq_starts_at(const vector<int>& bisquares, int diff, int n, int start) {
   int i = start;
   int target = bisquares[start];
   int seq_len = 0;
@@ -146,19 +61,34 @@ int seq_starts_at(vector<int>& bisquares, int diff, int n, int start) {
   return 1;
 }
 
+vector<int> possible_diffs(const vector<int>& bisquares, int start) {
+  vector<int> diffs(bisquares.size() - start + 1);
+  for (int i = start+1; i < bisquares.size(); ++i) {
+    diffs[i-start+1] = bisquares[i] - bisquares[start];
+  }
+  return diffs;
+}
+
 void solve_reverse(int n, int m, ofstream& fout) {
   int found_one = 0;
+  vector<pair<int,int>> ans;
+  ans.reserve(10000);
   auto bisquares = create_bisquares(m);
-  for (int diff = 1; diff < 2*m*m; ++diff) {
-    for (int start = 0; start < bisquares.size(); ++start) {
+  for (int start = 0; start < bisquares.size(); ++start) {
+    auto diffs = possible_diffs(bisquares, start);
+    for (auto& diff : diffs) {
       if (seq_starts_at(bisquares, diff, n, start)) {
-	found_one = 1;
-	fout << bisquares[start] << " " << diff << endl;
+	ans.push_back(make_pair(bisquares[start],diff));
       }
     }
   }
 
-  if (!found_one) {
+  sort(ans.begin(), ans.end(), cmp_pairs);
+  
+  for (auto &a : ans) {
+    fout << a.first << " " << a.second << endl;
+  }
+  if (ans.size() == 0) {
     fout << "NONE\n";
   }
   return;
@@ -171,7 +101,7 @@ int main() {
   int n, m;
   fin >> n >> m;
 
-  cout << n << " " << m << endl;
+  // cout << n << " " << m << endl;
   solve_reverse(n, m, fout);
 
   return 0;
