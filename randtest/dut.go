beb90f2c003c0ddaa0e5818cefe239d755a7eef0
Dan Luu <danluu@gmail.com>
2015-03-11 17:21:19 -0500

    Commit old changes.

--- randtest/dut.go
+++ randtest/dut.go
@@ -1,14 +1,26 @@
 package dut
 
+// Written this way to get the coverage tool to expand it.
+// Coverage tool doesn't currently expand && or ||
+func some_filter(x int) bool {
+	for i := 0; i < 16; i = i + 1 {
+		if !(x&1 == 1) {
+			return false
+		}
+		x >>= 1
+	}
+	return true
+}
+
 func dut(a []int) int {
 	if len(a) != 4 {
 		return 1
 	}
 
-	if a[0] > 1<<62 {
-		if a[1] > 1<<62 {
-			if a[2] > 1<<62 {
-				if a[3] > 1<<62 {
+	if some_filter(a[0]) {
+		if some_filter(a[1]) {
+			if some_filter(a[2]) {
+				if some_filter(a[3]) {
 					return 0
 				}
 				return 2
--- randtest/dut_test.go
+++ randtest/dut_test.go
@@ -1,11 +1,18 @@
 package dut
 
 import (
+	//	"fmt"
 	"testing"
 )
 
 func TestOnce(t *testing.T) {
 	if dut([]int{1, 2, 3, 4}) == 0 {
+		t.Errorf("Error from 1 2 3 4")
+	}
+}
+
+func TestOnceB(t *testing.T) {
+	if dut([]int{0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF}) == 0 {
 		t.Errorf("Error from 1 2 3 4")
 	}
 }
