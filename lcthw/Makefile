b10128e24b5abae62d6b5ae15a7d220cad0f46de
Dan Luu <danluu@gmail.com>
2015-06-18 20:41:51 -0700

    More LTCHW.

--- lcthw/Makefile
+++ lcthw/Makefile
@@ -1,4 +1,8 @@
 CFLAGS=-Wall -g
 
+all: ex19
+
+ex19: object.o
+
 clean:
-	rm -f ex1
+	rm -f ex19
---
+++ lcthw/object.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "object.h"
+#include <assert.h>
+
+void Object_destroy(void *self) {
+  Object *obj = self;
+
+  if (obj) {
+    if (obj->description) free(obj->description);
+    free(obj);
+  }
+}
+
+void Object_describe(void *self) {
+  Object *obj = self;
+  printf("%s.\n", obj->description);
+}
+
+int Object_init(void *self) {
+  // TODO: why does this even exist?
+  return 1;
+}
+
+void *Object_move(void *self, Direction direction) {
+  printf("direction fail!\n");
+  return NULL;
+}
+
+int Object_attack(void *self, int damage) {
+  printf("can't attack!\n");
+  return 0;
+}
+
+void *Object_new(size_t size, Object proto, char *description) {
+
+  if(!proto.init) proto.init = Object_init;
+  if(!proto.describe) proto.describe = Object_describe;
+  if(!proto.destroy) proto.destroy = Object_destroy;
+  if(!proto.attack) proto.attack = Object_attack;
+  if(!proto.move) proto.move = Object_move;
+
+  // this seems weird, but we can make a struct of one size,
+  // then point a different pointer at it to "cast" it
+  Object *el = calloc(1, size);
+  *el = proto;
+
+  // copy the description over
+  el->description = strdup(description);
+
+  // initialize it with whatever init we were given
+  if(!el->init(el)) {
+    // looks like it didn't initialize properly
+    el->destroy(el);
+    return NULL;
+  } else {
+    // all done, we made an object of any type
+    return el;
+  }
+}
