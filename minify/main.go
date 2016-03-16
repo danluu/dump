4346da16c233cd85ec29fba1822614bd448354ab
Dan Luu <danluu@gmail.com>
2016-03-15 21:01:40 -0700

    Add script to workaround html minifier weirdness.

---
+++ minify/main.go
@@ -0,0 +1,52 @@
+// html-minifier's recursive mode has some quirks.
+// This explicitly runs html-minifier only on files that are valid.
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+// TODO: fix link spacing so that we don't have to use --conservative-collapse.
+
+var (
+	srcDir   = "/home/dluu/dev/dist"
+	dstDir   = "/home/dluu/dev/dist"
+	baseArgs = "--remove-comments --collapse-whitespace --conservative-collapse --collapse-boolean-attributes --collapse-inline-tag-whitespace --remove-tag-whitespace --remove-attribute-quotes --use-short-doctype --remove-empty-attributes --remove-optional-tags --remove-empty-elements --minify-css"
+)
+
+func minifyFun(path string, info os.FileInfo, err error) error {
+
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+	if strings.HasSuffix(path, ".html") {
+		cmd := "html-minifier"
+		dstPath := dstDir + strings.TrimPrefix(path, srcDir)
+		fmt.Println(dstPath)
+		curArgs := baseArgs + " -o " + dstPath + " " + path
+		args := strings.Split(curArgs, " ")
+		cmdOut, err := exec.Command(cmd, args...).Output()
+		if err != nil {
+			log.Fatal("FAIL", path, err)
+		}
+
+		fmt.Println(string(cmdOut))
+	}
+
+	return nil
+}
+
+func main() {
+	fmt.Println("minify")
+	err := filepath.Walk(srcDir, minifyFun)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
