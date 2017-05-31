66cff06659dc48a4805bd256e1a36c050ac8d3fa
Dan Luu <danluu@gmail.com>
2017-05-31 17:33:28 -0400

    Try different cacheline sizes.

--- cache-params/src/cache.cc
+++ cache-params/src/cache.cc
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 #include <vector>
 
 #include "rdtsc.h"
 
 #define BUFFER_SIZE 1024 * 1024 * 128
 #define LINE_SIZE 128
+#define MAX_CACHE_SIZE 16 * 1024 * 1024
 
 uint64_t run_and_time_fn(std::vector<uint8_t>& buf, size_t len, int iterations, uint64_t(*fn)(const std::vector<uint8_t>&, size_t)) {
 
@@ -40,17 +43,42 @@ uint64_t naive_loop(const std::vector<uint8_t>& buf, size_t size) {
   return cnt;
 }
 
+template <typename T>
+std::string join(std::vector<T> const &v) {
+  std::stringstream ss;
+
+  for (size_t i = 0; i < v.size(); ++i) {
+    if (i != 0) {
+      ss << ",";
+    }
+    ss << v[i];
+  }
+
+  return ss.str();
+}
+
 int main() {
   std::vector<uint8_t> buf(BUFFER_SIZE);
 
-  const int iters = 32;
-  const size_t len = 16 * 1024;
-  uint64_t cycles = run_and_time_fn(buf, len, iters, naive_loop);
+  std::vector<size_t> sizes;
+  for (size_t s = LINE_SIZE; s <= MAX_CACHE_SIZE; s <<= 1) {
+    std::cout << s << std::endl;
+    sizes.push_back(s);
+  }
 
-  double num_loads = len / LINE_SIZE;
-  double cycles_per_load = cycles / num_loads;
+  const int iters = 4;
+  std::vector<double> cycles_per_load;
 
-  std::cout << cycles_per_load << std::endl;
+  for (const size_t len : sizes) {
+    uint64_t cycles = run_and_time_fn(buf, len, iters, naive_loop);
+
+    double num_loads = len / LINE_SIZE;
+    double cpl = cycles / num_loads;
+    cycles_per_load.push_back(cpl);
+  }
+
+  std::cout << join(sizes) << std::endl;
+  std::cout << join(cycles_per_load) << std::endl;
 
   return 0;
 }
