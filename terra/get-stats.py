5cb4967d5f21f2f5742b5d2c02971006c3380bad
Dan Luu <danluu@gmail.com>
2017-06-12 18:30:31 -0400

    Update join script for python3.

--- terra/get-stats.py
+++ terra/get-stats.py
@@ -2,18 +2,21 @@ import json
 import urllib.request
 import time
 
+# Gets summary data from terra.snellman.net. This does not have detailed game data.
+# It will tell you player, faction, start order, VPs,
+
 # First month: http://terra.snellman.net/app/results/v2/2013/02
-for year in range(2013, 2016):
-    for month in range(1, 12):
-        if year >= 2013 and month >= 2:
+for year in range(2013, 2018):
+    for month in range(1, 13):
+        if year > 2013 or month >= 2:
             urlpath = "http://terra.snellman.net/app/results/v2/{0}/{1}".format(year, month)
-            filepath = "{0}.{1}.json".format(year, month)
+            filepath = "summary-data/{0}.{1}.json".format(year, month)
             print(urlpath)
             urldata = urllib.request.urlopen(urlpath)
             jsondata = json.load(urldata)
             with open(filepath, 'w') as outfile:
                 json.dump(jsondata, outfile)
-            time.sleep(1)
+            time.sleep(0.1)
 
             
             
--- terra/join-stats.py
+++ terra/join-stats.py
@@ -1,6 +1,4 @@
 import json
-import urllib2
-import time
 
 def append_game_hash(all_games, filepath):
     with open(filepath) as json_file:
@@ -11,19 +9,16 @@ def append_game_hash(all_games, filepath):
 
 # First month: http://terra.snellman.net/app/results/v2/2013/02
 all_games = {}
-for year in range(2013, 2016):
-    for month in range(01, 12):
-        if (year >= 2013 and month >= 02):
-            filepath = "{0}.{1}.json".format(year, month)
-            print filepath
+for year in range(2013, 2018):
+    for month in range(1, 13):
+        if (year > 2013 or month >= 2):
+            filepath = "summary-data/{0}.{1}.json".format(year, month)
+            print(filepath)
             append_game_hash(all_games, filepath)
-            print len(all_games)
+            print(len(all_games))
 
 with open('all_games.json', 'w') as outfile:
     json.dump(all_games, outfile) 
-                
-            
-            
 
 # j = json.load(data)
 # print j
