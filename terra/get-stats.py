ddc37788321b3f354dd811939273ddaeefc6af86
Dan Luu <danluu@gmail.com>
2017-06-12 17:02:46 -0400

    Convert terra.snellman.net stats download script to Python3.

--- terra/get-stats.py
+++ terra/get-stats.py
@@ -1,15 +1,15 @@
 import json
-import urllib2
+import urllib.request
 import time
 
 # First month: http://terra.snellman.net/app/results/v2/2013/02
 for year in range(2013, 2016):
-    for month in range(01, 12):
-        if (year >= 2013 and month >= 02):
+    for month in range(1, 12):
+        if year >= 2013 and month >= 2:
             urlpath = "http://terra.snellman.net/app/results/v2/{0}/{1}".format(year, month)
             filepath = "{0}.{1}.json".format(year, month)
-            print urlpath
-            urldata = urllib2.urlopen(urlpath)
+            print(urlpath)
+            urldata = urllib.request.urlopen(urlpath)
             jsondata = json.load(urldata)
             with open(filepath, 'w') as outfile:
                 json.dump(jsondata, outfile)
