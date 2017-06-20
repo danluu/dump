b188e35c540399d89cdd07320d35441e08fc3996
Dan Luu <danluu@gmail.com>
2017-06-20 14:02:04 -0400

    Graph tweaks.

--- terra/plot-wins-vs-faction.r
+++ terra/plot-wins-vs-faction.r
@@ -20,18 +20,45 @@ library(gridSVG)
 #    theme_minimal()
 # gridSVG::grid.export("wins-vs-faction-adjusted.svg")
 
-data <- read.csv(file="faction.top.csv", header=TRUE, sep=",")
-ggplot(data, aes(rating, win.rate)) +
-   geom_point(aes(colour=factor(faction), shape=factor(faction)), size = 6, position=position_dodge(width = 30)) +
-   scale_colour_manual(values = c("orange","black","brown","black","orange","grey","brown","green","purple")) +
-   scale_shape_manual(values = c(19, 19, 19, 13, 13, 19, 13, 19, 19)) +
-   theme_minimal()
-gridSVG::grid.export("wins-vs-faction-top.svg")
+# data <- read.csv(file="faction.top.csv", header=TRUE, sep=",")
+# ggplot(data, aes(rating, win.rate)) +
+#    geom_point(aes(colour=factor(faction), shape=factor(faction)), size = 6, position=position_dodge(width = 30)) +
+#    scale_colour_manual(values = c("orange","black","brown","black","orange","grey","brown","green","purple")) +
+#    scale_shape_manual(values = c(19, 19, 19, 13, 13, 19, 13, 19, 19)) +
+#    theme_minimal()
+# gridSVG::grid.export("wins-vs-faction-top.svg")
 
-data <- read.csv(file="faction.bot.csv", header=TRUE, sep=",")
+# data <- read.csv(file="faction.bot.csv", header=TRUE, sep=",")
+# ggplot(data, aes(rating, win.rate)) +
+#    geom_point(aes(colour=factor(faction), shape=factor(faction)), size = 6, position=position_dodge(width = 30)) +
+#    scale_colour_manual(values = c("green","red","grey","yellow","red","purple","blue","yellow","blue")) +
+#    scale_shape_manual(values = c(19, 19, 19, 19, 13, 19, 19, 13, 13)) +
+#    theme_minimal()
+# gridSVG::grid.export("wins-vs-faction-bot.svg")
+
+data <- read.csv(file="faction.0.csv", header=TRUE, sep=",")
 ggplot(data, aes(rating, win.rate)) +
-   geom_point(aes(colour=factor(faction), shape=factor(faction)), size = 6, position=position_dodge(width = 30)) +
-   scale_colour_manual(values = c("green","red","grey","yellow","red","purple","blue","yellow","blue")) +
-   scale_shape_manual(values = c(19, 19, 19, 19, 13, 19, 19, 13, 13)) +
+   geom_point(aes(colour=faction, shape=faction), size = 6, position=position_dodge(width = 30)) +
+   geom_line(aes(group=faction, colour=faction), position=position_dodge(width=30)) + 
+   scale_colour_manual(values = c("orange","black","brown","black","orange","grey","brown","green","purple")) +
+   scale_shape_manual(values = c(19, 19, 19,13, 13, 19, 13, 19, 19)) +
    theme_minimal()
-gridSVG::grid.export("wins-vs-faction-bot.svg")
+gridSVG::grid.export("wins-vs-faction-0.svg")
+
+data <- read.csv(file="faction.1.csv", header=TRUE, sep=",")
+ggplot(data, aes(rating, win.rate)) +
+   geom_point(aes(colour=faction, shape=faction), size = 6, position=position_dodge(width = 30)) +
+   geom_line(aes(group=faction, colour=faction), position=position_dodge(width=30)) + 
+   scale_colour_manual(values = c("red","brown","purple","blue","yellow","blue")) + 
+   scale_shape_manual(values = c(19, 19, 19, 19, 19, 13)) +
+   theme_minimal()
+gridSVG::grid.export("wins-vs-faction-1.svg")
+
+data <- read.csv(file="faction.2.csv", header=TRUE, sep=",")
+ggplot(data, aes(rating, win.rate)) +
+   geom_point(aes(colour=faction, shape=faction), size = 6, position=position_dodge(width = 30)) +
+   geom_line(aes(group=faction, colour=faction), position=position_dodge(width=30)) + 
+   scale_colour_manual(values = c("green","yellow","red")) + 
+   scale_shape_manual(values = c(19, 19, 19)) +
+   theme_minimal()
+gridSVG::grid.export("wins-vs-faction-2.svg")
