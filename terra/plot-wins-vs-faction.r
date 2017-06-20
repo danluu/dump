8ec824510a3b0107a3ed79016a51a9d51f64b89b
Dan Luu <danluu@gmail.com>
2017-06-20 19:17:46 -0400

    Fix formatting typo.

--- terra/plot-wins-vs-faction.r
+++ terra/plot-wins-vs-faction.r
@@ -41,7 +41,7 @@ ggplot(data, aes(rating, win.rate)) +
    geom_point(aes(colour=faction, shape=faction), size = 6, position=position_dodge(width = 30)) +
    geom_line(aes(group=faction, colour=faction), position=position_dodge(width=30)) + 
    scale_colour_manual(values = c("orange","black","brown","black","orange","grey","brown","green","purple")) +
-   scale_shape_manual(values = c(19, 19, 19,13, 13, 19, 13, 19, 19)) +
+   scale_shape_manual(values = c(19, 19, 19, 13, 13, 19, 13, 19, 19)) +
    theme_minimal()
 gridSVG::grid.export("wins-vs-faction-0.svg")
 
