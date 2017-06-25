17a121975ff4535cf94bc25f648a4cdf5a1c2dd1
Dan Luu <danluu@gmail.com>
2017-06-24 21:28:35 -0400

    Add more weights.

--- weight/plot-weight.r
+++ weight/plot-weight.r
@@ -3,5 +3,6 @@ library(ggplot2)
 data <- read.csv(file="weights.csv", header=TRUE, sep=",")
 ggplot(data, aes(date, weight)) +
   geom_point(aes(colour=status)) +
-  theme_minimal()
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
   
