7412be137adfee4b495c29ddb8040afed14fb7a1
Jaseem Abid <jaseemabid@gmail.com>
2017-07-06 14:38:28 -0400

    Delete arcane clobber list

--- cache-params/src/rdtsc.h
+++ cache-params/src/rdtsc.h
@@ -3,21 +3,12 @@
 // See http://akaros.cs.berkeley.edu/lxr/akaros/kern/arch/x86/rdtsc_test.c
 // See https://stackoverflow.com/questions/38994549/is-intels-timestamp-reading-asm-code-example-using-two-more-registers-than-are
 
-#ifdef __i386__
-#  define RDTSC_DIRTY "%eax", "%edx"
-#elif __x86_64__
-#  define RDTSC_DIRTY "%rax", "%rdx"
-#else
-# error unknown platform
-#endif
 
 #define RDTSC(cycles)                                      \
     do {                                                   \
         unsigned cyc_high, cyc_low;                        \
         asm volatile("LFENCE\n\t"                          \
                      "RDTSC\n\t"                           \
-                     : "=d" (cyc_high), "=a" (cyc_low)     \
-                     :: RDTSC_DIRTY);                      \
+                     : "=d" (cyc_high), "=a" (cyc_low));                       \
         (cycles) = ((uint64_t)cyc_high << 32) | cyc_low;   \
     } while (0)
-
