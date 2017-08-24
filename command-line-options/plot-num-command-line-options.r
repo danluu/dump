25ae17b340d51cdc63468aa3ab8394725d8d5391
Dan Luu <danluu@gmail.com>
2017-08-24 15:24:47 -0400

    Add quick script to plot log-log histogram.

---
+++ command-line-options/plot-num-command-line-options.r
@@ -0,0 +1,8 @@
+library(ggplot2)
+library(scales)
+
+data <- read.csv(file="all.results", header=TRUE, sep=",")
+
+ggplot(data=data, aes(data$num_options)) + stat_density(aes(y=..count..), color="black", fill="blue", alpha=0.3) + scale_x_continuous(brea
+ks=c(0,1,2,4,8,16,32,64,128,256,512,1024), trans="log1p", expand=c(0,0)) + scale_y_continuous(breaks=c(0,128,1024,16384), trans="log1p", exp
+and=c(0,0))
