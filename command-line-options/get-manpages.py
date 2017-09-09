3516a5c98b9b078bb7b9a080d50638f424197f58
Dan Luu <danluu@gmail.com>
2017-09-09 15:01:32 -0400

    Add results from zesty.

--- command-line-options/get-manpages.py
+++ command-line-options/get-manpages.py
@@ -1,7 +1,9 @@
 import re
+import subprocess
+
 
 def get_available_mans():
-    names = []
+    names = set()
     
     filepattern = re.compile('a href="(.*)\.1\.gz"')
 
@@ -9,10 +11,34 @@ def get_available_mans():
         for line in f:
             matched = filepattern.search(line)
             if matched != None:
-                names.append(matched.group(1))
+                names.add(matched.group(1))
     return names
 
 
-names = get_available_mans()
-print(names)
+def get_commands():
+    commands = []
+
+    with open('popular-cortesi.csv') as f:
+        for line in f:
+            commands.append(line.strip())
+
+    return commands
+
+def find_man_path(name):
+    result = subprocess.check_output(['find', '/usr/share/man/man1', '-name', '{}.*gz'.format(name)])
+    return result.decode("utf-8").strip()
+
+    
+
+# names = get_available_mans()
+commands = get_commands()
+
+for c in commands:
+    path = find_man_path(c)
+    if path == "":
+        print("FAIL",c)
+    else:
+        print(path)
+    
+    
 
