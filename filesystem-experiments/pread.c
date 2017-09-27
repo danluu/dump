d0823de3a033962f2e8fcc2a0881b7adce2c2b21
Dan Luu <danluu@gmail.com>
2017-09-27 14:30:55 -0400

    Print out errno when available.

--- filesystem-experiments/pread.c
+++ filesystem-experiments/pread.c
@@ -1,4 +1,7 @@
+#include <errno.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -11,6 +14,7 @@ int main() {
   ssize_t rcode = pread(fd, buf, 100, 0);
 
   if (rcode < 0) {
+    printf("errno %s\n", strerror(errno));
     return rcode;
   } else {
     return 0;
