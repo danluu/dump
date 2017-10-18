3f58c2327110a51eb107b66bf363b95b3b29dbc7
Dan Luu <danluu@gmail.com>
2017-10-18 16:19:55 -0400

    Add hack script to generate html table.

---
+++ html-table/make-table.py
@@ -0,0 +1,32 @@
+import csv
+
+# input_path = 'filesystems.csv'
+input_path = 'small-filesystems.csv'
+with open(input_path) as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    body = [line for line in reader]
+
+# TODO: if this script is ever re-used, don't hardcode color config.
+color_map = {'':'black',
+             'prop':'olive',
+             'fix':'green',
+             'ignore':'red'}
+
+print('<style>table {border-collapse:collapse;margin:0px auto;}table,th,td {border: 1px solid black;}td {text-align:center;}</style>')
+
+print('<table>')
+print('<tr>')
+for hitem in header:
+    print('<th>{}</th>'.format(hitem))
+print('</tr>')
+
+for row in body:
+    print('<tr>')
+    for bitem in row:
+        if bitem in color_map:
+            print('<td bgcolor={}>{}</td>'.format(color_map[bitem],bitem))            
+        else:
+            print('<td>{}</td>'.format(bitem))
+    print('</tr>')
+print('</table>')
