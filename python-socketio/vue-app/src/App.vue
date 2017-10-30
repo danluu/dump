58fba656acae178fbae2d16e1d1dcd79f2cd17d9
Dan Luu <danluu@gmail.com>
2017-10-30 16:53:44 -0400

    Add client-side ping.

--- python-socketio/vue-app/src/App.vue
+++ python-socketio/vue-app/src/App.vue
@@ -1,13 +1,43 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <p v-if="isConnected">We're connected to the server!</p>
+    <p>Message from server: "{{socketMessage}}"</p>
+    <button @click="pingServer()">Ping Server</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      isConnected: false,
+      socketMessage: ''
+    }
+  },
+
+  sockets: {
+    connect () {
+      // Fired when the socket connects.
+      this.isConnected = true
+    },
+
+    disconnect () {
+      this.isConnected = false
+    },
+
+    // Fired when the server sends something on the "messageChannel" channel.
+    messageChannel (data) {
+      this.socketMessage = data
+    }
+  },
+
+  methods: {
+    pingServer () {
+      // Send the "pingServer" event to the server.
+      this.$socket.emit('pingServer', 'PING!')
+    }
+  }
 }
 </script>
 
