138da41499580a24c52c09303fb43b30cd009a51
Dan Luu <danluu@gmail.com>
2017-10-30 17:26:40 -0400

    Show connect/disconnect info.

--- python-socketio/main.py
+++ python-socketio/main.py
@@ -9,9 +9,25 @@ socketio = SocketIO(app)
 def index():
     return render_template('index.html')
 
+@socketio.on('connect')
+def test_connect():
+    print('got connection')
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('got disconnection')
+
 @socketio.on('my event')
 def test_message(message):
     emit('my response', {'data': 'got it!'})
 
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
+    send('got message: ' + message)
+
 if __name__ == '__main__':
+    print('running socketio')
     socketio.run(app, port=4113)
+    print('ran socketio')
