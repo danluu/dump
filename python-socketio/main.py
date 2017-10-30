6fcdd23f041156977c1a07421b5cb971a5fefade
Dan Luu <danluu@gmail.com>
2017-10-29 22:26:20 -0400

    Add basic socketio example.

---
+++ python-socketio/main.py
@@ -0,0 +1,30 @@
+from aiohttp import web
+import socketio
+
+sio = socketio.AsyncServer()
+app = web.Application()
+sio.attach(app)
+
+async def index(request):
+    """Serve the client-side application."""
+    with open('index.html') as f:
+        return web.Response(text=f.read(), content_type='text/html')
+
+@sio.on('connect', namespace='/chat')
+def connect(sid, environ):
+    print("connect ", sid)
+
+@sio.on('chat message', namespace='/chat')
+async def message(sid, data):
+    print("message ", data)
+    await sio.emit('reply', room=sid)
+
+@sio.on('disconnect', namespace='/chat')
+def disconnect(sid):
+    print('disconnect ', sid)
+
+app.router.add_static('/static', 'static')
+app.router.add_get('/', index)
+
+if __name__ == '__main__':
+    web.run_app(app, port=4113)
