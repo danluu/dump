1420b4090867a78acca576aeb0997e84db2c1dc9
Dan Luu <danluu@gmail.com>
2017-10-30 17:21:53 -0400

    More fixup after changing style.

--- python-socketio/vue-app/src/main.js
+++ python-socketio/vue-app/src/main.js
@@ -1,8 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueSocketIO from 'vue-socket.io';
+import socketio from 'socket.io-client/dist/socket.io';
+
 import App from './App';
 import router from './router';
+
+export const SocketInstance = socketio('http://localhost:4113');
+export const foo = 'foo';
+
+Vue.use(VueSocketIO, SocketInstance);
 
 Vue.config.productionTip = false;
 
