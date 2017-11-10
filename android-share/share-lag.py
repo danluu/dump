2b8041a0f8ac89c40e2cbdc85570d44701a3c260
Dan Luu <danluu@gmail.com>
2017-11-10 15:05:59 -0500

    Transform share data to age.

--- android-share/share-lag.py
+++ android-share/share-lag.py
@@ -1,10 +1,18 @@
 import collections
 import csv
 import datetime
-
+import math
 
 release_date = {}
 share = collections.defaultdict(dict)
+
+average_month = 30.44
+first_date = datetime.datetime.strptime('2009-02-09', '%Y-%m-%d')
+last_date = datetime.datetime.strptime('2017-10-02', '%Y-%m-%d')
+date_range = last_date - first_date
+max_buckets = math.ceil((date_range.days + 1) / average_month)
+
+buckets = collections.defaultdict(lambda: [0.0] * max_buckets)
 
 with open('version-history.csv') as vh_file:
     vh_reader = csv.reader(vh_file)
@@ -23,5 +31,20 @@ with open('historical-share.csv') as hs_file:
         date = datetime.datetime.strptime(row['date'], "%Y-%m-%d")
         for version in row:
             if version != 'date':
-                share[date][version] = row[version]
+                share[date][version] = float(row[version])
 
+for date in share:
+    for version in share[date]:
+        age = date - release_date[version]
+        current_share = share[date][version]
+        if age < datetime.timedelta(0):
+            assert(current_share == 0.0)
+            continue
+        if current_share == 0.0:
+            continue
+
+        min_bidx = math.floor(age.days / average_month)
+        for i in range(min_bidx, max_buckets -1):
+            buckets[date][i] += current_share
+
+print(buckets)
