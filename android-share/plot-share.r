d1982a2f2b85551fefff469157f7df8c6483d356
Dan Luu <danluu@gmail.com>
2017-11-10 17:05:30 -0500

    Convert date strings to dates.

--- android-share/plot-share.r
+++ android-share/plot-share.r
@@ -1,4 +1,4 @@
 library(ggplot2)
 
-df <- read.csv("share-plot.csv")
-ggplot(df, aes(x=date,y=percent,color=months)) + geom_point()
+df <- read.csv("share-plot.csv",colClasses=c("Date",NA,NA))
+ggplot(df, aes(x=date,y=percent,color=as.factor(months))) + geom_line()
