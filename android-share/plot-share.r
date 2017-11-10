e7f3146e45154973840b000bbc1f7ce7807acb84
Dan Luu <danluu@gmail.com>
2017-11-10 17:37:09 -0500

    Change color scale to kinda work.

--- android-share/plot-share.r
+++ android-share/plot-share.r
@@ -1,4 +1,5 @@
 library(ggplot2)
 
 df <- read.csv("share-plot.csv",colClasses=c("Date",NA,NA))
-ggplot(df, aes(x=date,y=percent,fill=as.factor(months))) + geom_area()
+ ggplot(df, aes(x=date,y=percent,fill=as.factor(months))) + geom_area() + scale_fill_manual(values = colorRampPalette(c("darkgreen","goldenrod3","goldenrod2","goldenrod1
+","firebrick4","firebrick3","firebrick2","firebrick1"))(104))
