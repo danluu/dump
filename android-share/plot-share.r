6a3c81df6b5ce23b397ec82c656bba85c96ce63d
Dan Luu <danluu@gmail.com>
2017-11-12 15:43:34 -0500

    Adjust legend, text size, etc.

--- android-share/plot-share.r
+++ android-share/plot-share.r
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-svg(filename="android-age.svg")
+svg(filename="android-age.svg", width=16, height=16)
 
 df <- read.csv("share-plot.csv",colClasses=c("Date",NA,NA))
 new_pal <- colorRampPalette(c("darkblue","darkgreen"))(3)
@@ -8,9 +8,11 @@ recent_pal <- colorRampPalette(c("forestgreen","goldenrod4"))(3)
 real_pal <- colorRampPalette(c("goldenrod1","darkorange2","darkorange4","firebrick4","firebrick2","white","gray87","gray67","gray48","gray28","gray11","black"))(74)
 extra_pal <- colorRampPalette(c("black","black"))(24)
 my_pal <- c(new_pal, recent_pal, real_pal, extra_pal)
-ggplot(df, aes(x=date,y=percent,fill=as.factor(months))) + geom_area() +
+ggplot(na.omit(df), aes(x=date,y=percent,fill=as.factor(months))) + geom_area() +
+           theme_minimal() +
+           theme(text = element_text(size = 15)) +
            scale_fill_manual(values = my_pal, guide="colorbar") +
-           guides(fill=guide_legend(keyheight=0.01,nrow=74)) +
-           theme(legend.text = element_text(size=5))
+           guides(fill=guide_legend(keyheight=0.5, ncol=1, title="months")) +
+           labs(y = "marketshare")
 
 dev.off()
