89b54a207335fd9290ae2f9719d8c797d8912a31
Dan Luu <danluu@gmail.com>
2017-11-12 15:21:35 -0500

    Shrink legend.

--- android-share/plot-share.r
+++ android-share/plot-share.r
@@ -7,6 +7,9 @@ new_pal <- colorRampPalette(c("darkblue","darkgreen"))(3)
 real_pal <- colorRampPalette(c("forestgreen","goldenrod3","goldenrod1","darkorange2","darkorange4","firebrick4","firebrick2","white","gray87","gray67","gray48","gray28","gray11","black"))(77)
 extra_pal <- colorRampPalette(c("black","black"))(24)
 my_pal <- c(new_pal,real_pal, extra_pal)
-ggplot(df, aes(x=date,y=percent,fill=as.factor(months))) + geom_area() + scale_fill_manual(values = my_pal)
+ggplot(df, aes(x=date,y=percent,fill=as.factor(months))) + geom_area() +
+           scale_fill_manual(values = my_pal, guide="colorbar") +
+           guides(fill=guide_legend(keyheight=0.01,nrow=74)) +
+           theme(legend.text = element_text(size=5))
 
 dev.off()
