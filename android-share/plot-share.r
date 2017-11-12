81960322ca414a1fba4f3a33370717687c264750
Dan Luu <danluu@gmail.com>
2017-11-12 14:23:13 -0500

    Output svg nstead of png.

--- android-share/plot-share.r
+++ android-share/plot-share.r
@@ -1,9 +1,9 @@
 library(ggplot2)
 
-png(filename="android-age.png",width=1920,height=1080)
+svg(filename="android-age.svg")
 
 df <- read.csv("share-plot.csv",colClasses=c("Date",NA,NA))
-my_pal <- colorRampPalette(c("darkgreen","goldenrod3","goldenrod2","goldenrod1","firebrick4","firebrick3","firebrick2","firebrick1"))(80)
+my_pal <- colorRampPalette(c("darkgreen","goldenrod3","goldenrod2","goldenrod1","firebrick4","firebrick3","firebrick2","firebrick1","gray87","gray67","gray48","gray28","gray11","black"))(80)
 ggplot(df, aes(x=date,y=percent,fill=as.factor(months))) + geom_area() + scale_fill_manual(values = my_pal)
 
 dev.off()
