0f399b47b253cc536c7a178e3c4659434b59159a
Dan Luu <danluu@gmail.com>
2017-11-12 13:18:34 -0500

    Extract palette generation.

--- android-share/plot-share.r
+++ android-share/plot-share.r
@@ -1,4 +1,5 @@
 library(ggplot2)
 
 df <- read.csv("share-plot.csv",colClasses=c("Date",NA,NA))
- ggplot(df, aes(x=date,y=percent,fill=as.factor(months))) + geom_area() + scale_fill_manual(values = colorRampPalette(c("darkgreen","goldenrod3","goldenrod2","goldenrod1","firebrick4","firebrick3","firebrick2","firebrick1"))(80))
+my_pal <- colorRampPalette(c("darkgreen","goldenrod3","goldenrod2","goldenrod1","firebrick4","firebrick3","firebrick2","firebrick1"))(80)
+ggplot(df, aes(x=date,y=percent,fill=as.factor(months))) + geom_area() + scale_fill_manual(values = my_pal)
