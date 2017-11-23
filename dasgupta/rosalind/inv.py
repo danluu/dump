3b50e36968041e58790be3b3cae61065cc5e79f0
Dan Luu <danluu@gmail.com>
2017-11-23 12:35:24 -0500

    Count inversions.

--- dasgupta/rosalind/inv.py
+++ dasgupta/rosalind/inv.py
@@ -1,6 +1,12 @@
+num_swaps = 0
+
 def merge(raw0, raw1):
+    global num_swaps
+
     a0 = ms(raw0)
     a1 = ms(raw1)
+
+    # print('{}:{}'.format(a0, a1))
     
     res = []
 
@@ -17,6 +23,7 @@ def merge(raw0, raw1):
         elif a1[i1] < a0[i0]:
             res.append(a1[i1])
             i1 += 1
+            num_swaps += len(a0) - i0
         else:
             res.append(a0[i0])
             i0 += 1
@@ -30,9 +37,12 @@ def ms(arr):
     mid = len(arr) // 2
     return merge(arr[:mid],arr[mid:])
 
-with open('rosalind_inv.txt') as f:
+# with open('rosalind_inv.txt') as f:
+with open('/home/danluu/Downloads/rosalind_inv.txt') as f:
     num_entries = int(f.readline().strip())
     arr = [int(x) for x in f.readline().strip().split(' ')]
 
-print(arr)
-print(ms(arr))
+# print(arr)
+# print(ms(arr))
+ms(arr)
+print(num_swaps)
