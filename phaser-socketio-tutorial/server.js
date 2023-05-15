3779137e294a30864d62396e84498c26b33699f7
Dan Luu <danluu@gmail.com>
2023-05-14 21:02:36 -0700

    Send events on connect/disconnect.

--- phaser-socketio-tutorial/server.js
+++ phaser-socketio-tutorial/server.js
@@ -4,6 +4,8 @@ var server = require('http').Server(app);
 
 var io = require('socket.io')(server);
 
+var players = {};
+
 app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -11,8 +13,26 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
     console.log('a user connected');
+
+    // create a new player and add it to our players object
+    players[socket.id] = {
+        rotation: 0,
+        x: Math.floor(Math.random() * 700) + 50,
+        y: Math.floor(Math.random() * 500) + 50,
+        playerId: socket.id,
+        team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+    };
+    // send the players object to the new player
+    socket.emit('currentPlayers', players);
+    // update all other players of the new player
+    socket.broadcast.emit('newPlayer', players[socket.id]);
+    
     socket.on('disconnect', function () {
       console.log('user disconnected');
+
+      delete players[socket.id];
+      // Tutorial says to send 'disconnect' but that errors out because 'disconnect' is reserved.
+      io.emit('player-dc', socket.id);
     });
   });
 
