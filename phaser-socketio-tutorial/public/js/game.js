13dcdbcd48bea72e06fbe3d8fea94000eda24a4e
Dan Luu <danluu@gmail.com>
2023-05-17 16:47:26 -0700

    Display player.

--- phaser-socketio-tutorial/public/js/game.js
+++ phaser-socketio-tutorial/public/js/game.js
@@ -18,10 +18,75 @@ var config = {
   };
   var game = new Phaser.Game(config);
 
-  function preload() {}
-
-  function create() {
-    this.socket = io();
+  function preload() {
+    this.load.image('ship', 'assets/spaceShips_001.png');
   }
 
-  function update() {}
+  function create() {
+    var self = this;
+    this.socket = io();
+    this.socket.on('currentPlayers', function (players) {
+        Object.keys(players).forEach(function (id) {
+          if (players[id].playerId === self.socket.id) {
+            addPlayer(self, players[id]);
+          }
+        });
+      });
+/*
+      this.socket.on('newPlayer', function (playerInfo) {
+        addOtherPlayers(self, playerInfo);
+      });
+      
+      this.socket.on('disconnect', function (playerId) {
+        self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+          if (playerId === otherPlayer.playerId) {
+            otherPlayer.destroy();
+          }
+        });
+      });  
+      
+      this.cursors = this.input.keyboard.createCursorKeys(); */     
+  }
+
+  function update() {/*
+    if (this.ship) {
+        if (this.cursors.left.isDown) {
+          this.ship.setAngularVelocity(-150);
+        } else if (this.cursors.right.isDown) {
+          this.ship.setAngularVelocity(150);
+        } else {
+          this.ship.setAngularVelocity(0);
+        }
+      
+        if (this.cursors.up.isDown) {
+          this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
+        } else {
+          this.ship.setAcceleration(0);
+        }
+      
+        this.physics.world.wrap(this.ship, 5);
+      }*/
+  }
+
+  function addPlayer(self, playerInfo) {
+    self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+    if (playerInfo.team === 'blue') {
+      self.ship.setTint(0x0000ff);
+    } else {
+      self.ship.setTint(0xff0000);
+    }
+    self.ship.setDrag(100);
+    self.ship.setAngularDrag(100);
+    self.ship.setMaxVelocity(200);
+  }
+
+  function addOtherPlayers(self, playerInfo) {
+    const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'otherPlayer').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+    if (playerInfo.team === 'blue') {
+      otherPlayer.setTint(0x0000ff);
+    } else {
+      otherPlayer.setTint(0xff0000);
+    }
+    otherPlayer.playerId = playerInfo.playerId;
+    self.otherPlayers.add(otherPlayer);
+  }
