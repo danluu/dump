f871f88e210fd6741d9c10046b21e7e021bf7480
Dan Luu <danluu@gmail.com>
2023-05-25 00:46:53 -0700

    1-34

--- essential-typescript-4-exercises/todo/src/index.ts
+++ essential-typescript-4-exercises/todo/src/index.ts
@@ -10,15 +10,16 @@ let todos: TodoItem[] = [
     new TodoItem(4, "Call Joe", true)];
 
 let collection: TodoCollection = new TodoCollection("Adam", todos);
+let showCompleted = true;
 
 function displayTodoList(): void {
     console.log(`${collection.userName}'s Todo List `
         + `(${ collection.getItemCounts().incomplete } items to do)`);
-    collection.getTodoItems(true).forEach(item =>
-item.printDetails());
+    collection.getTodoItems(showCompleted).forEach(item => item.printDetails());
 }
  
 enum Commands {
+    Toggle = "Show/Hide Completed",
     Quit = "Quit"
 }
 
@@ -31,10 +32,13 @@ function promptUser(): void {
             message: "Choose option",
             choices: Object.values(Commands)
     }).then(answers => {
-        if (answers["command"] !== Commands.Quit) {
-            promptUser();
+        switch (answers["command"]) {
+            case Commands.Toggle:
+                showCompleted = !showCompleted;
+                promptUser();
+                break;
         }
-}) 
+    })
 }
 
 promptUser();
