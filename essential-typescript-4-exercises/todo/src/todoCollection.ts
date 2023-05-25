6cddb0fc6030c4a7b98b66247322e1a37bec9789
Dan Luu <danluu@gmail.com>
2023-05-24 19:10:52 -0700

    1-12

---
+++ essential-typescript-4-exercises/todo/src/todoCollection.ts
@@ -0,0 +1,27 @@
+import { TodoItem } from "./todoItem";
+
+export class TodoCollection {
+    private nextId: number = 1;
+
+    constructor(public userName: string, public todoItems: TodoItem[] = []) {
+        // no statements required
+    }
+
+    addTodo(task: string): number {
+        while (this.getTodoById(this.nextId)) {
+            this.nextId++;
+        }
+        this.todoItems.push(new TodoItem(this.nextId, task));
+        return this.nextId;
+    }
+
+    getTodoById(id: number) : TodoItem {
+        return this.todoItems.find(item => item.id === id); }
+
+    markComplete(id: number, complete: boolean) {
+        const todoItem = this.getTodoById(id);
+        if (todoItem) {
+            todoItem.complete = complete;
+        }
+    } 
+}
