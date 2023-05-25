8b0a8869203081e2a007d8b234506ac126830aca
Dan Luu <danluu@gmail.com>
2023-05-24 19:07:26 -0700

    1-11

--- essential-typescript-4-exercises/todo/src/todoItem.ts
+++ essential-typescript-4-exercises/todo/src/todoItem.ts
@@ -1,12 +1,7 @@
 export class TodoItem {
-    public id: number;
-    public task: string;
-    public complete: boolean = false;
-
-    public constructor(id: number, task: string, complete: boolean = false) {
-        this.id = id;
-        this.task = task;
-        this.complete = complete;
+    public constructor(public id: number, 
+        public task: string, 
+        public complete: boolean = false) {
     }
 
     public printDetails(): void {
