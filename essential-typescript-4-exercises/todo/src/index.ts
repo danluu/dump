b81f2e106d2bf7a654870abb505dee4bd686747b
Dan Luu <danluu@gmail.com>
2023-05-24 19:18:02 -0700

    1-16

--- essential-typescript-4-exercises/todo/src/index.ts
+++ essential-typescript-4-exercises/todo/src/index.ts
@@ -1,17 +1,18 @@
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
 
-let todos = [
+let todos: TodoItem[] = [
     new TodoItem(1, "Buy Flowers"),
     new TodoItem(2,"Get Shoes"),
     new TodoItem(3, "Collect Tickets"),
     new TodoItem(4, "Call Joe", true)];
 
-let collection = new TodoCollection("Adam", todos);
+let collection: TodoCollection = new TodoCollection("Adam", todos);
 
 console.clear();
 console.log(`${collection.userName}'s Todo List`);
 
-let newId = collection.addTodo("Go for run");
-let todoItem = collection.getTodoById(newId);
-console.log(JSON.stringify(todoItem));
+let newId: number = collection.addTodo("Go for run");
+let todoItem: TodoItem = collection.getTodoById(newId);
+collection.getTodoById(newId);
+todoItem.printDetails();
