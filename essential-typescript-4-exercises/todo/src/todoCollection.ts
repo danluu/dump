3132d722eeae928a5c10573bdc10ca5a378fa800
Dan Luu <danluu@gmail.com>
2023-05-24 19:20:09 -0700

    1-18

--- essential-typescript-4-exercises/todo/src/todoCollection.ts
+++ essential-typescript-4-exercises/todo/src/todoCollection.ts
@@ -2,21 +2,23 @@ import { TodoItem } from "./todoItem";
 
 export class TodoCollection {
     private nextId: number = 1;
+    private itemMap = new Map<number, TodoItem>();
 
     constructor(public userName: string, public todoItems: TodoItem[] = []) {
-        // no statements required
+        todoItems.forEach(item => this.itemMap.set(item.id, item));
     }
 
     addTodo(task: string): number {
         while (this.getTodoById(this.nextId)) {
             this.nextId++;
         }
-        this.todoItems.push(new TodoItem(this.nextId, task));
+        this.itemMap.set(this.nextId, new TodoItem(this.nextId, task));
         return this.nextId;
     }
 
     getTodoById(id: number) : TodoItem {
-        return this.todoItems.find(item => item.id === id); }
+        return this.itemMap.get(id); 
+    }
 
     markComplete(id: number, complete: boolean) {
         const todoItem = this.getTodoById(id);
