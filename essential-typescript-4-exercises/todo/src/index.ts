7ac1e285e64e41ea5212038c3db9a06eae22a808
Dan Luu <danluu@gmail.com>
2023-05-24 20:03:37 -0700

    Fix ESM import error.

--- essential-typescript-4-exercises/todo/src/index.ts
+++ essential-typescript-4-exercises/todo/src/index.ts
@@ -1,5 +1,7 @@
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
+import * as inquirer from 'inquirer';
+// import inquirer from 'inquirer';
 
 let todos: TodoItem[] = [
     new TodoItem(1, "Buy Flowers"),
@@ -9,12 +11,30 @@ let todos: TodoItem[] = [
 
 let collection: TodoCollection = new TodoCollection("Adam", todos);
 
-console.clear();
-console.log(`${collection.userName}'s Todo List `
-    + `(${ collection.getItemCounts().incomplete } items to do)`);
-let newId: number = collection.addTodo("Go for run");
-let todoItem: TodoItem = collection.getTodoById(newId);
+function displayTodoList(): void {
+    console.log(`${collection.userName}'s Todo List `
+        + `(${ collection.getItemCounts().incomplete } items to do)`);
+    collection.getTodoItems(true).forEach(item =>
+item.printDetails());
+}
+ 
+enum Commands {
+    Quit = "Quit"
+}
 
-collection.removeComplete();
+function promptUser(): void {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+            type: "list",
+            name: "command",
+            message: "Choose option",
+            choices: Object.values(Commands)
+    }).then(answers => {
+        if (answers["command"] !== Commands.Quit) {
+            promptUser();
+        }
+}) 
+}
 
-collection.getTodoItems(true).forEach(item => item.printDetails());
+promptUser();
