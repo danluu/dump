9a0a21b0577f352a9afb17bb1c6e00f846dc2691
Dan Luu <danluu@gmail.com>
2023-05-27 23:48:22 -0700

    1-44

--- essential-typescript-4-exercises/todo/src/index.ts
+++ essential-typescript-4-exercises/todo/src/index.ts
@@ -1,7 +1,9 @@
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
 import * as inquirer from 'inquirer';
-// import inquirer from 'inquirer';
+import { JsonTodoCollection } from "./jsonTodoCollection";
+
+// To execute: tsc && node dist/index.js
 
 let todos: TodoItem[] = [
     new TodoItem(1, "Buy Flowers"),
@@ -9,7 +11,7 @@ let todos: TodoItem[] = [
     new TodoItem(3, "Collect Tickets"),
     new TodoItem(4, "Call Joe", true)];
 
-let collection: TodoCollection = new TodoCollection("Adam", todos);
+let collection: TodoCollection = new JsonTodoCollection("Adam", todos);
 let showCompleted = true;
 
 function displayTodoList(): void {
--- essential-typescript-4-exercises/todo/src/todoCollection.ts
+++ essential-typescript-4-exercises/todo/src/todoCollection.ts
@@ -7,7 +7,7 @@ type ItemCounts = {
 
 export class TodoCollection {
     private nextId: number = 1;
-    private itemMap = new Map<number, TodoItem>();
+    protected itemMap = new Map<number, TodoItem>();
 
     constructor(public userName: string, public todoItems: TodoItem[] = []) {
         todoItems.forEach(item => this.itemMap.set(item.id, item));
