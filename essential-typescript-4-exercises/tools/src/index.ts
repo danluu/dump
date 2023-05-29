8f250a9dfc26ad203107f9e46778b56b1abdec30
Dan Luu <danluu@gmail.com>
2023-05-28 20:31:29 -0700

    6-11

--- essential-typescript-4-exercises/tools/src/index.ts
+++ essential-typescript-4-exercises/tools/src/index.ts
@@ -1,6 +1,13 @@
+import { sum } from "./calc";
+
 function printMessage(msg: string): void {
     console.log(`Message: ${msg}`);
 
 }
 
 printMessage("Hello, TypeScript");
+
+debugger;
+
+const total = sum(100, 200, 300);
+console.log(`Total: ${total}`);
