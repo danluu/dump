783e3a7360daa47d0f23f45bf8dd1f282fe243ce
Dan Luu <danluu@gmail.com>
2023-05-28 20:13:09 -0700

    5-6

---
+++ essential-typescript-4-exercises/primer/index.js
@@ -0,0 +1,14 @@
+let hatPrice = 100;
+console.log(`Hat price: ${hatPrice}`);
+
+let bootsPrice = "100";
+console.log(`Boots price: ${bootsPrice}`);
+
+if (hatPrice == bootsPrice) {
+    console.log("Prices are the same");
+} else {
+    console.log("Prices are different");
+}
+
+let totalPrice = Number(hatPrice) + Number(bootsPrice);
+console.log(`Total Price: ${totalPrice}`);
---
+++ essential-typescript-4-exercises/tools/src/index.ts
@@ -0,0 +1,6 @@
+function printMessage(msg: string): void {
+    console.log(`Message: ${msg}`);
+
+}
+
+printMessage("Hello, TypeScript");
