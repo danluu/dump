377044adaa837a8298d23e761f21eee0742d7f16
Dan Luu <danluu@gmail.com>
2023-05-29 12:42:25 -0700

    14-11

--- essential-typescript-4-exercises/usingjs/src/index.ts
+++ essential-typescript-4-exercises/usingjs/src/index.ts
@@ -1,15 +1,17 @@
 import { SportsProduct, SPORT } from "./product";
 import { Cart } from "./cart";
+import { sizeFormatter, costFormatter } from "./formatters";
+
 let kayak = new SportsProduct(1, "Kayak", 275,
 SPORT.Watersports);
 let hat =  new SportsProduct(2, "Hat", 22.10,
 SPORT.Running, SPORT.Watersports);
-let ball = new SportsProduct(3, "Soccer Ball",
-19.50, SPORT.Soccer);
+let ball = new SportsProduct(3, "Soccer Ball", 19.50, SPORT.Soccer);
+
 let cart = new Cart("Bob");
 cart.addProduct(kayak, 1);
 cart.addProduct(hat, 1);
 cart.addProduct(hat, 2);
-console.log(`Cart has ${cart.itemCount} items`);
-console.log(`Cart value is
-$${cart.totalPrice.toFixed(2)}`);
+
+sizeFormatter("Cart", cart.itemCount);
+costFormatter("Cart", cart.totalPrice);
