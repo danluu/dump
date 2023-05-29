623677f6018f044584a387b6e61c244b8abef5d9
Dan Luu <danluu@gmail.com>
2023-05-29 16:04:23 -0700

    15-5

--- essential-typescript-4-exercises/webapp/src/index.ts
+++ essential-typescript-4-exercises/webapp/src/index.ts
@@ -1,19 +1,23 @@
 import { LocalDataSource } from "./data/localDataSource";
+import { DomDisplay } from "./domDisplay"; import "bootstrap/dist/css/bootstrap.css";
 
-async function displayData(): Promise<string> {
-    let ds = new LocalDataSource();
-    let allProducts = await ds.getProducts("name");
-    let categories = await ds.getCategories();
-    let chessProducts = await ds.getProducts("name", "Chess");
+let ds = new LocalDataSource();
 
-    let result = "";
-
-    allProducts.forEach(p => result += `Product: ${p.name}, ${p.category}\n`);
-    categories.forEach(c => result += (`Category: ${c}\n`));
-    chessProducts.forEach(p => ds.order.addProduct(p, 1));
-    result += `Order total: $${ds.order.total.toFixed(2)}`;
-
-    return result;    
+async function displayData(): Promise<HTMLElement> {
+    let display = new DomDisplay();
+    display.props = {
+        products: await ds.getProducts("name"),
+        order: ds.order
+    }
+    return display.getContent();
 }
 
-displayData().then(res => console.log(res));
+document.onreadystatechange = () => {
+    if (document.readyState === "complete") {
+        displayData().then(elem => {
+            let rootElement = document.getElementById("app");
+            rootElement.innerHTML = "";
+            rootElement.appendChild(elem);
+        });
+    } 
+};
--- essential-typescript-4-exercises/webapp/webpack.config.js
+++ essential-typescript-4-exercises/webapp/webpack.config.js
@@ -3,10 +3,11 @@ module.exports = {
     devtool: "inline-source-map",
     entry: "./src/index.ts",
     output: { filename: "bundle.js" },
-    resolve: { extensions: [".ts", ".js"] },
+    resolve: { extensions: [".ts", ".js", ".css"] },
     module: {
         rules: [
-            { test: /\.ts/, use: "ts-loader", exclude: /node_modules/ }
+            { test: /\.ts/, use: "ts-loader", exclude: /node_modules/ },
+            { test: /\.css$/, use: ["style-loader", "css-loader"] },
         ]
     },
     devServer: {
