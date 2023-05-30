b1acc3b5fb8cf601fdc6058bac3ee7f197dc9ba2
Dan Luu <danluu@gmail.com>
2023-05-29 23:52:01 -0700

    16-22

---
+++ essential-typescript-4-exercises/webapp/src/data/remoteDataSource.ts
@@ -0,0 +1,22 @@
+import { AbstractDataSource } from "./abstractDataSource";
+import { Product, Order } from "./entities";
+import Axios from "axios";
+
+const protocol = "http";
+const hostname = "localhost";
+const port = 4600;
+
+const urls = {
+    products: `${protocol}://${hostname}:${port}/products`,
+    orders: `${protocol}://${hostname}:${port}/orders`
+}
+
+export class RemoteDataSource extends AbstractDataSource {
+    loadProducts(): Promise<Product[]> {
+        return Axios.get(urls.products).then(response => response.data);
+    }
+
+    storeOrder(): Promise<number> {
+        return Axios.post(urls.orders, this.order).then(response => response.data.id);
+    }
+}
--- essential-typescript-4-exercises/webapp/src/htmlDisplay.tsx
+++ essential-typescript-4-exercises/webapp/src/htmlDisplay.tsx
@@ -2,10 +2,17 @@ import { createElement } from "./tools/jsxFactory";
 import { Product, Order } from "./data/entities";
 import { AbstractDataSource } from "./data/abstractDataSource";
 import { ProductList } from "./productList";
+import { Header } from "./header";
+import { OrderDetails } from "./orderDetails";
+import { Summary } from "./summary";
+
+enum DisplayMode { List, Details, Complete }
 
 export class HtmlDisplay {
     private containerElem: HTMLElement;
     private selectedCategory: string;
+    private mode: DisplayMode = DisplayMode.List;
+    private orderId: number;
 
     constructor() {
         this.containerElem = document.createElement("div");
@@ -24,13 +31,33 @@ export class HtmlDisplay {
         let products = await this.props.dataSource.getProducts("id", this.selectedCategory);
         let categories = await this.props.dataSource.getCategories();
         this.containerElem.innerHTML = "";
-        let content = <div>
-            <ProductList products={ products } categories={ categories }
-                selectedCategory={ this.selectedCategory }
-                addToOrderCallback={ this.addToOrder }
-                filterCallback={ this.selectCategory } />
-                </div>
-                this.containerElem.appendChild(content);
+        let contentElem: HTMLElement;
+        switch (this.mode) {
+            case DisplayMode.List:
+                contentElem = this.getListContent(products, categories);
+                break;
+            case DisplayMode.Details:
+                contentElem = <OrderDetails order={this.props.dataSource.order}
+                    cancelCallback={this.showList}
+                    submitCallback={this.submitOrder} />
+                break;
+            case DisplayMode.Complete:
+                contentElem = <Summary orderId={this.orderId}
+                    callback= {this.showList} />
+                break;
+        }
+        this.containerElem.appendChild(contentElem);        
+    }
+
+    getListContent(products: Product[], categories: string[]): HTMLElement {
+        return <div>
+            <Header order={this.props.dataSource.order}
+                submitCallback={this.showDetails} />
+            <ProductList products={products} categories={categories}
+                selectedCategory={this.selectedCategory}
+                addToOrderCallback={this.addToOrder}
+                filterCallback={this.selectCategory} />
+        </div>
     }
 
     addToOrder = (product: Product, quantity: number): void => {
@@ -41,5 +68,24 @@ export class HtmlDisplay {
     selectCategory = (category: string): void => {
         this.selectedCategory = category === "All" ? undefined : category;
         this.updateContent();
+    }
+
+    showDetails = (): void => {
+        this.mode = DisplayMode.Details;
+        this.updateContent();
+    }
+
+    showList = (): void => {
+        this.mode = DisplayMode.List;
+        this.updateContent();
+    }
+
+    submitOrder = async (): Promise<void> => {
+        this.props.dataSource.storeOrder().then(id => {
+            this.orderId = id;
+            this.props.dataSource.order = new Order();
+            this.mode = DisplayMode.Complete;
+            this.updateContent();
+        });
     }
 }
