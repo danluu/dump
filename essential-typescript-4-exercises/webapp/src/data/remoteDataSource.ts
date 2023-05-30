b876c201a8e217e23f8eb3ff2a11ded8c820db23
Dan Luu <danluu@gmail.com>
2023-05-30 00:02:06 -0700

    16-28

--- essential-typescript-4-exercises/webapp/src/data/remoteDataSource.ts
+++ essential-typescript-4-exercises/webapp/src/data/remoteDataSource.ts
@@ -2,13 +2,15 @@ import { AbstractDataSource } from "./abstractDataSource";
 import { Product, Order } from "./entities";
 import Axios from "axios";
 
-const protocol = "http";
-const hostname = "localhost";
-const port = 4600;
+// const protocol = "http";
+// const hostname = "localhost";
+// const port = 4600;
 
 const urls = {
-    products: `${protocol}://${hostname}:${port}/products`,
-    orders: `${protocol}://${hostname}:${port}/orders`
+    // products: `${protocol}://${hostname}:${port}/products`,
+    // orders: `${protocol}://${hostname}:${port}/orders`
+    products: "/api/products",
+    orders: "/api/orders"    
 }
 
 export class RemoteDataSource extends AbstractDataSource {
