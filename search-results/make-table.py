b9b893c15b685335539df93e08e70e2f5679565f
Dan Luu <danluu@gmail.com>
2023-12-29 20:43:38 -0800

    Tweak wording.

--- search-results/make-table.py
+++ search-results/make-table.py
@@ -1,37 +1,29 @@
 import csv
 
-# input_path = 're24.csv'
 input_path = 're24-wide.csv'
 with open(input_path) as f:
     reader = csv.reader(f)
     header = next(reader)
     body = [line for line in reader]
 
+entry_values = {'Terrible', 'V. Bad', 'Bad', 'Ok', 'Good', 'Great'}
+
 def flip_text_color(color):
     return color == '#67001f' or color == '#053061'
 
 def value_to_color(value):
-    colors = ['#67001f','#b2182b','#d6604d','#f4a582','#fddbc7','#f7f7f7','#d1e5f0','#92c5de','#4393c3','#2166ac','#053061']
+    lookup = {
+        'Terrible': '#053061',
+        'V. Bad': '#4393c3',
+        'Bad': '#d1e5f0',
+        'Ok': '#f7f7f7',
+        'Good': '#d6604d',
+        'Great': '#67001f',
+    }
+    # colors = ['#67001f','#b2182b','#d6604d','#f4a582','#fddbc7','#f7f7f7','#d1e5f0','#92c5de','#4393c3','#2166ac','#053061']
 
-    min_val = .117
-    mid_val = .555
-    max_val = 2.417
+    return lookup[value]
 
-    mid_idx = 5
-    max_idx = len(colors) - 1
-
-    if value <= mid_val:
-        idx = int((mid_idx / (mid_val - min_val)) * (value - min_val))
-    else:
-        idx = int(((max_idx - mid_idx) / (max_val - mid_val)) * (value - mid_val)) + mid_idx
-
-    assert(idx >= 0)
-    assert(idx <= max_idx)
-
-    return colors[idx]
-
-
-    
 
 print('<style>table {border-collapse:collapse;margin:0px auto;}table,th,td {border: 1px solid black;}td {text-align:center;}</style>')
 
@@ -47,14 +39,13 @@ print('<tr><th>bases</th><th colspan="3">outs</th></tr>')
 for row in body:
     print('<tr>')
     for bitem in row:
-        try:
-            value = float(bitem)
-            color = value_to_color(value)
+        if bitem in entry_values:
+            color = value_to_color(bitem)
             if flip_text_color(color):
                 print('<td bgcolor={}><font color=white>{}</font></td>'.format(color, bitem),end='')
             else:
                 print('<td bgcolor={}>{}</td>'.format(color, bitem),end='')
-        except ValueError:
+        else:
             print('<th>{}</th>'.format(bitem),end='')
 
     print('</tr>')
