f2a63acf3e41a068e9bb7cd28984585b2f4be6ad
Dan Luu <danluu@gmail.com>
2024-02-04 00:11:16 -0800

    Create weird janky object.

--- php-zandstra/c3/example.php
+++ php-zandstra/c3/example.php
@@ -5,17 +5,63 @@ class ShopProduct
 {
     public function __construct(
         public string $title, 
-        public string $producerFirstName = "",
-        public string $producerMainName = "", 
-        public float $price = 0.0) {}
+        public string $producerFirstName ,
+        public string $producerMainName, 
+        public float $price,
+        int $numPages = 0,
+        float $playLength = 0
+        ) {}
 
 
-    public function getProducer()
+    public function getProducer(): string
     {
         return "{$this->producerFirstName}" . " {$this->producerMainName}";
     }
+
+    public function getSummaryLine(): string
+    {
+        $base = "{$this->title} ({$this->producerMainName}, ";
+        $base .= "{$this->producerFirstName})";
+        return $base;
+    }
     
 }
+
+class CdProduct extends ShopProduct {
+    public function getPlayLength(): int {
+        return $this->playLength;
+    }
+
+    public function getSummaryLine(): string {
+        $base = "{$this->title} ({$this->producerMainName}, ";
+        $base .= "{$this->producerFirstName})";
+        $base .= ": playing time - {$this->playLength}";
+        return $base;
+    }
+}
+
+class BookProduct extends ShopProduct {
+    public function getNumberOfPages(): int {
+        return $this->numPages;
+    }
+
+    public function getSummaryLine(): string
+    {
+        $base = "{$this->title} ({$this->producerMainName}, ";
+        $base .= "{$this->producerFirstName})";
+        $base .= ": page count - {$this->numPages}";
+        return $base;
+    }
+}
+
+$product2 = new CdProduct(
+    "Exile on Coldharbour Lane",
+    "The",
+    "Alabama 3",
+    10.99,
+    0,
+    60.33
+);
 
 class ShopProductWriter {
     public function write(ShopProduct $shopProduct) {
@@ -25,24 +71,6 @@ class ShopProductWriter {
 
 }
 
-$product1 = new ShopProduct("My Antonia", "Willa", "Cather", 5.99);
-$product2 = new ShopProduct(
-    title: "Shop Catalog",
-    price: 0.7
-);
-
-$product1->title = "My Antonia";
-// $product2->title = "Catch 22";
-
-
-print "author: {$product1->getProducer()}\n";
-
-// var_dump($product1);
-var_dump($product2);
-
-$writer = new ShopProductWriter();
-$writer->write($product1);
-$writer->write($product2);
 
 class AddressManager {
     private $addresses = ["209.131.36.159", "216.58.213.174"];
@@ -61,5 +89,11 @@ class AddressManager {
 $settings = simplexml_load_file("resolve.xml");
 $manager = new AddressManager();
 $manager->outputAddresses(filter_var($settings->resolvedomains, FILTER_VALIDATE_BOOLEAN));
+
+class Storage{
+    public function add(string $key, string|bool $value) {
+
+    }
+}
 
 ?> 
