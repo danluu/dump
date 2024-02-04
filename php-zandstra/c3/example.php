c281dcab824d3737bf973b276c57c5a7f027d8ad
Dan Luu <danluu@gmail.com>
2024-02-03 22:14:25 -0800

    Add method to class.

--- php-zandstra/c3/example.php
+++ php-zandstra/c3/example.php
@@ -2,13 +2,29 @@
 
 class ShopProduct
 {
-    // class body
+    public $title = "default product";
+    public $producerMainName = "main name";
+    public $producerFirstName = "first name";
+    public $price = 0;
+
+    public function getProducer()
+    {
+        return "{$this->producerFirstName}" . " {$this->producerMainName}";
+    }
+    
 }
 
 $product1 = new ShopProduct();
 $product2 = new ShopProduct();
 
-var_dump($product1);
-var_dump($product2);
+$product1->title = "My Antonia";
+$product2->title = "Catch 22";
+
+print "author: {$product1->producerFirstName} " . "{$product1->producerMainName}\n";
+
+print "author: {$product1->getProducer()}\n";
+
+// var_dump($product1);
+// var_dump($product2);
 
 ?> 
