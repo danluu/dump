9e5ea99ac5960563bd7c8af5ff172d8df24b4ba0
Dan Luu <danluu@gmail.com>
2024-02-09 17:27:58 -0800

    Update.

---
+++ php-slim-tutorial/config/middleware.php
@@ -0,0 +1,16 @@
+<?php
+
+use Slim\App;
+
+return function (App $app) {
+    // Parse json, form data and xml
+    $app->addBodyParsingMiddleware();
+
+    // Add the Slim built-in routing middleware
+    $app->addRoutingMiddleware();
+
+    // Handle exceptions
+    $app->addErrorMiddleware(true, true, true);
+};
+
+?>
---
+++ php-slim-tutorial/config/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+use Slim\App;
+
+return function (App $app) {
+    // empty
+};
+
+?>
