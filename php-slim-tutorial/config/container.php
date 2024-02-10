da8754fc9b7fd00f21fba5d12380dd8edf288b51
Dan Luu <danluu@gmail.com>
2024-02-09 17:12:13 -0800

    Add DI container app.

---
+++ php-slim-tutorial/config/container.php
@@ -0,0 +1,23 @@
+<?php
+
+use Psr\Container\ContainerInterface;
+use Slim\App;
+use Slim\Factory\AppFactory;
+
+return [
+    'settings' => function () {
+        return require __DIR__ . '/settings.php';
+    },
+
+    App::class => function (ContainerInterface $container) {
+        $app = AppFactory::createFromContainer($container);
+
+        // Register routes
+        (require __DIR__ . '/routes.php')($app);
+
+        // Register middleware
+        (require __DIR__ . '/middleware.php')($app);
+
+        return $app;
+    },
+];
