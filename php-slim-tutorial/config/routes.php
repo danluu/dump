8538486a80657146ffd43384402bfc97a9334ab4
Dan Luu <danluu@gmail.com>
2024-02-09 17:33:15 -0800

    Add hello world response.

--- php-slim-tutorial/config/routes.php
+++ php-slim-tutorial/config/routes.php
@@ -1,9 +1,13 @@
 <?php
 
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Slim\App;
 
 return function (App $app) {
-    // empty
-};
-
+    $app -> get('/', function (ServerRequestInterface $request, ResponseInterface $response) {
+        $response -> getBody() -> write('Hello, World!');
+        return $response;
+    });
+}
 ?>
