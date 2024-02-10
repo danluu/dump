9252211c6a5a04fa3b9b062aadda2dda0faa4d59
Dan Luu <danluu@gmail.com>
2024-02-09 17:23:09 -0800

    Add boostrap.

---
+++ php-slim-tutorial/config/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+use DI\ContainerBuilder;
+use Slim\App;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Build DI container instance
+$container = (new ContainerBuilder())
+    ->addDefinitions(__DIR__ . '/container.php')
+    ->build();
+
+// Create App instance
+return $container->get(App::class);
+
+?>
