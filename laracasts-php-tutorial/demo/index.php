fe5a3d95113943a0d7359f433be4409da408df22
Dan Luu <danluu@gmail.com>
2024-02-10 19:02:34 -0800

    Add associative array.

--- laracasts-php-tutorial/demo/index.php
+++ laracasts-php-tutorial/demo/index.php
@@ -18,11 +18,27 @@
 <body>
     <h1><Recommended Books/h1>
 
-    <?php $books = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]; ?>
+    <?php $books = [
+        [
+            'title' => 'The Pragmatic Programmer',
+            'author' => 'Andrew Hunt, David Thomas',
+            'extra' => 'This is a great book for any programmer'
+        ],
+        [
+            'title' => 'Clean Code',
+            'author' => 'Robert C. Martin',
+            'extra' => 'Hmmmm'
+        ],
+        [
+            'title' => 'Cracking the Coding Interview',
+            'author' => 'Gayle Laakmann McDowell',
+            'extra' => '$'
+        ]
+    ]; ?>
 
     <ul>
         <?php foreach($books as $book): ?>
-            <li><?= $book ?></li>
+            <li><?= $book['author'] . ": " . $book['title'] ?></li>
         <?php endforeach; ?> 
     </ul>
 </body>
