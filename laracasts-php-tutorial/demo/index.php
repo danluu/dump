da8e698375875c6a09d6e93f05635679ba76b417
Dan Luu <danluu@gmail.com>
2024-02-10 18:18:38 -0800

    Add styling.

--- laracasts-php-tutorial/demo/index.php
+++ laracasts-php-tutorial/demo/index.php
@@ -4,6 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
+    <style>
+        body{
+            height: 100vh;
+            margin: 0;
+            display: grid;
+            font-family: sans-serif;
+            place-items: center;
+        }
+        </style>
 </head>
 
 <body>
