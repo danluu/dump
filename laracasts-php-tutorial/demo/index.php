2c52f3a9752edb2d37d54c8cf9c52413ad069b28
Dan Luu <danluu@gmail.com>
2024-02-11 11:09:21 -0800

    Move code to top.

--- laracasts-php-tutorial/demo/index.php
+++ laracasts-php-tutorial/demo/index.php
@@ -1,24 +1,4 @@
-<!doctype html>
-
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Demo</title>
-    <style>
-        body{
-            height: 100vh;
-            margin: 0;
-            display: grid;
-            font-family: sans-serif;
-            place-items: center;
-        }
-        </style>
-</head>
-
-<body>
-    <h1><Recommended Books/h1>
-
-    <?php $books = [
+<?php $books = [
         [
             'title' => 'The Pragmatic Programmer',
             'author' => 'Andrew Hunt, David Thomas',
@@ -47,6 +27,28 @@
 
 
     ?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+    <style>
+        body{
+            height: 100vh;
+            margin: 0;
+            display: grid;
+            font-family: sans-serif;
+            place-items: center;
+        }
+        </style>
+</head>
+
+<body>
+    <h1><Recommended Books/h1>
+
+ 
 
     <ul>
         <?php foreach($filteredBooks as $book): ?>
