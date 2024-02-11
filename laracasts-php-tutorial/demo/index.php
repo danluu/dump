18a233fd8a763ec47efb333beca4f3c76b9dbced
Dan Luu <danluu@gmail.com>
2024-02-11 11:14:24 -0800

    Load view

--- laracasts-php-tutorial/demo/index.php
+++ laracasts-php-tutorial/demo/index.php
@@ -21,39 +21,12 @@
     ]        
 ];
 
-    $filteredBooks = array_filter($books, function($book) {
-        return $book['author'] !== 'Gayle Laakmann McDowell';
-    });
+$filteredBooks = array_filter($books, function($book) {
+    return $book['author'] !== 'Gayle Laakmann McDowell';
+});
+
+require 'index.view.php';
+
+?>
 
 
-    ?>
-
-    <!doctype html>
-
-    <html lang="en">
-    <head>
-    <meta charset="UTF-8">
-    <title>Demo</title>
-    <style>
-        body{
-            height: 100vh;
-            margin: 0;
-            display: grid;
-            font-family: sans-serif;
-            place-items: center;
-        }
-    </style>
-    </head>
-
-    <body>
-    <h1><Recommended Books/h1>
-
-
-
-    <ul>
-        <?php foreach($filteredBooks as $book): ?>
-        <li><?= $book['author'] . ": " . $book['title'] ?></li>
-        <?php endforeach; ?> 
-    </ul>
-    </body>
-    </html>
