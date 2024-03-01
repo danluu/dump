424e47d6be8f47e1315ceaf46dd8555061ba2138
Dan Luu <danluu@gmail.com>
2024-02-29 19:26:54 -0800

    Put footer into partial.

--- laracasts-php-tutorial/2-dwa/views/about.view.php
+++ laracasts-php-tutorial/2-dwa/views/about.view.php
@@ -1,24 +1,6 @@
-<!doctype html>
-<html lang="en" class="h-full bg-gray-100">
-<head>
-    <meta charset="UTF-8">
-    <title>Demo</title>
-    <script src="https://cdn.tailwindcss.com/"></script>
-</head>
-<body class="h-full">
-<!--
-  This example requires updating your template:
 
-  ```
-  <html class="h-full bg-gray-100">
-  <body class="h-full">
-  ```
--->
-<div class="min-h-full">
-
+<?php require('partials/head.php'); ?>
 <?php require('partials/nav.php'); ?>
-  
-
   <header class="bg-white shadow">
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
       <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
@@ -29,8 +11,4 @@
       <p>About page!?!</p>
     </div>
   </main>
-</div>
-
-</body>
-
-</html>
+  <?php require('partials/footer.php'); ?>  
--- laracasts-php-tutorial/2-dwa/views/contacts.view.php
+++ laracasts-php-tutorial/2-dwa/views/contacts.view.php
@@ -1,20 +1,4 @@
-<!doctype html>
-<html lang="en" class="h-full bg-gray-100">
-<head>
-    <meta charset="UTF-8">
-    <title>Demo</title>
-    <script src="https://cdn.tailwindcss.com/"></script>
-</head>
-<body class="h-full">
-<!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-gray-100">
-  <body class="h-full">
-  ```
--->
-<div class="min-h-full">
+<?php require('partials/head.php'); ?>
 <?php require('partials/nav.php'); ?>
 
   <header class="bg-white shadow">
@@ -27,8 +11,4 @@
       <p>Contacts?</p>
     </div>
   </main>
-</div>
-
-</body>
-
-</html>
+  <?php require('partials/footer.php'); ?>  
