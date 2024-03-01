487f5ee10f3f5cfe7e9be7570c86d2d988382acd
Dan Luu <danluu@gmail.com>
2024-02-29 19:29:02 -0800

    Put banner into partial.

--- laracasts-php-tutorial/2-dwa/views/about.view.php
+++ laracasts-php-tutorial/2-dwa/views/about.view.php
@@ -1,11 +1,6 @@
-
 <?php require('partials/head.php'); ?>
 <?php require('partials/nav.php'); ?>
-  <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-    </div>
-  </header>
+<?php require('partials/banner.php'); ?>
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
       <p>About page!?!</p>
--- laracasts-php-tutorial/2-dwa/views/index.view.php
+++ laracasts-php-tutorial/2-dwa/views/index.view.php
@@ -1,14 +1,9 @@
 <?php require('partials/head.php'); ?>
-  <?php require('partials/nav.php'); ?>
-
-  <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-    </div>
-  </header>
+<?php require('partials/nav.php'); ?>
+<?php require('partials/banner.php'); ?>
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
       <p>Hello. Welcome to the home page.</p>
     </div>
   </main>
-  <?php require('partials/footer.php'); ?>  
+<?php require('partials/footer.php'); ?>  
