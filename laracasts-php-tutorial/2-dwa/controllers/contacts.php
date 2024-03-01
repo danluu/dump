6693162c70970b048dd095cb261d45a711e84dd2
Dan Luu <danluu@gmail.com>
2024-02-29 20:15:43 -0800

    Move controllers into dir.

--- laracasts-php-tutorial/2-dwa/controllers/contacts.php
+++ laracasts-php-tutorial/2-dwa/controllers/contacts.php
@@ -1,6 +1,5 @@
 <?php
 
-require "functions.php";
 
 $heading = 'Contact';
 
--- laracasts-php-tutorial/2-dwa/index.php
+++ laracasts-php-tutorial/2-dwa/index.php
@@ -3,4 +3,16 @@ require "functions.php";
 
 $heading = 'Home';
 
+$uri = $_SERVER['REQUEST_URI'];
+
+if ($uri === '/') {
+    require "views/index.view.php";
+} elseif ($uri === '/about') {
+    require "views/about.view.php";
+} elseif ($uri === '/contacts') {
+    require "views/contacts.view.php";
+} else {
+    require "views/404.view.php";
+}
+
 require "views/index.view.php";
