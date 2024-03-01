e5e03800feac9f67afbcfe176b8468b14a6c2708
Dan Luu <danluu@gmail.com>
2024-02-29 20:31:51 -0800

    Move router to file.

--- laracasts-php-tutorial/2-dwa/index.php
+++ laracasts-php-tutorial/2-dwa/index.php
@@ -1,22 +1,3 @@
 <?php
 require "functions.php";
-
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/contacts' => 'controllers/contacts.php'
-];
-
-function abort($code = 404) {
-    http_response_code($code);
-    require "views/{$code}.view.php";
-    die();    
-}
-
-if (array_key_exists($uri, $routes)) {
-    require $routes[$uri];
-} else {
-    abort();
-}
+require "router.php";
---
+++ laracasts-php-tutorial/2-dwa/router.php
@@ -0,0 +1,24 @@
+<?php
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contacts' => 'controllers/contacts.php'
+];
+
+function abort($code = 404) {
+    http_response_code($code);
+    require "views/{$code}.view.php";
+    die();    
+}
+
+function routeToController($uri, $routes) {
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abort();
+    }
+}
+
+routeToController($uri, $routes);
